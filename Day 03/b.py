0a7e5e73d42fb3cbb2fc7f5ae0cbcb4323f37388
Kevin Knott <KKnoTT93@gmail.com>
2021-06-03 20:54:10 -0500

    Adding 3 problems and updating the README as I have only been able to solve about three problems a day

---
+++ Day 03/b.py
@@ -0,0 +1,16 @@
+# Kth Largest Element in an Array: https://leetcode.com/problems/kth-largest-element-in-an-array/
+# Given an integer array nums and an integer k, return the kth largest element in the array.
+# Note that it is the kth largest element in the sorted order, not the kth distinct element.
+
+
+class initial():
+    def findKthLargest(self,  nums, k):
+        return
+
+
+# Score Card
+# Did I need hints?
+# Did you finish within 30 min?
+# Was the solution optimal?
+# Were there any bugs?
+#  3 3 4 3 = 3.25
