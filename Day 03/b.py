50ae43fba2002065c941809fc8c24def929da11c
Kevin Knott <KKnoTT93@gmail.com>
2021-06-03 22:18:11 -0500

    Redoing this problem but I worked on a more optimal solution that uses a mix of quick sort and binary search

--- Day 03/b.py
+++ Day 03/b.py
@@ -7,6 +7,7 @@
 # Also previously just created a heap
 
 import heapq
+import random
 
 
 class initial():
@@ -22,6 +23,40 @@ class initial():
 
         return heapq._heappop_max(nums)
 
+    # New method you can technically quick sort! because if you quick sort you can choose a range like binary sort
+    # plus if your switch point ends at the kth index you just return the result!
+
+    def findKthLargestQS(self, nums, k):
+        def helper(start, end):
+            pivot = random.randint(start, end)
+            nums[pivot], nums[end] = nums[end], nums[pivot]
+            pivot = end
+
+            slow = start
+            for i in range(start, end):
+                if nums[i] < nums[pivot]:
+                    nums[i], nums[slow] = nums[slow], nums[i]
+                    slow += 1
+
+            nums[slow], nums[pivot] = nums[pivot], nums[slow]
+            return slow  # (nums, slow)
+
+        # aka binary search
+        def selection(target, start=0, end=len(nums)-1):
+            if start == end:
+                return nums[start]
+
+            random_pivot = helper(start, end)
+
+            if target > random_pivot:
+                return selection(target, random_pivot + 1, end)
+            elif target < random_pivot:
+                return selection(target, start, random_pivot - 1)
+            else:
+                return nums[random_pivot]
+
+        return selection(len(nums) - k)
+
 # Score Card
 # Did I need hints?
 # Did you finish within 30 min?
@@ -31,6 +66,6 @@ class initial():
 
 
 a = [3, 2, 1, 5, 6, 4]
-k = 6
+k = 3
 sol = initial()
-print(sol.findKthLargestHeap(a, k))
+print(sol.findKthLargestQS(a, k))
