756526c593d5f3b0bb4ea45f9d433fbd03e86e0b
Kevin Knott <KKnott93@gmail.com>
2021-06-04 21:19:41 -0500

    Setup initial problem

---
+++ Day 04/a.py
@@ -0,0 +1,15 @@
+# Two Sum: https://leetcode.com/problems/two-sum/
+# Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+# You may assume that each input would have exactly one solution, and you may not use the same element twice.
+# You can return the answer in any order.
+
+class init():
+    def twoSum(nums, k):
+        return
+
+# Score Card
+# Did I need hints?
+# Did you finish within 30 min?
+# Was the solution optimal?
+# Were there any bugs?
+#  1 1 2 2 = 1.5
---
+++ Day 04/b.py
@@ -0,0 +1,13 @@
+# Intersection of Two Arrays: https://leetcode.com/problems/intersection-of-two-arrays/
+# Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must be unique and you may return the result in any order.
+
+class init():
+    def intersection(nums1, nums2):
+        return
+
+# Score Card
+# Did I need hints?
+# Did you finish within 30 min?
+# Was the solution optimal?
+# Were there any bugs?
+#  1 1 2 2 = 1.5
