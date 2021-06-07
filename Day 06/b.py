ea59cacaedb436139587dfa17a1f85bb62059cb4
Kevin Knott <KKnott93@gmail.com>
2021-06-06 22:19:28 -0500

    First problem of day 6 a neat tree problem involving grabbing the right most node in tree

---
+++ Day 06/b.py
@@ -0,0 +1,16 @@
+# Convert Binary Search Tree to Sorted Doubly Linked List:
+# Convert a Binary Search Tree to a sorted Circular Doubly-Linked List in place.
+# You can think of the left and right pointers as synonymous to the predecessor and successor pointers in a doubly-linked list. For a circular doubly linked list, the predecessor of the first element is the last element, and the successor of the last element is the first element.
+# We want to do the transformation in place. After the transformation, the left pointer of the tree node should point to its predecessor, and the right pointer should point to its successor. You should return the pointer to the smallest element of the linked list.
+
+
+class init():
+    def treeToDoublyList(self, root):
+        return
+
+# Score Card
+# Did I need hints? N
+# Did you finish within 30 min? Y
+# Was the solution optimal? Y (although not realistic for interview)
+# Were there any bugs? N
+#  5 5 5 5 = 5
---
+++ Day 06/c.py
@@ -0,0 +1,15 @@
+#  Invert Binary Tree:  https://leetcode.com/problems/invert-binary-tree/
+# Given the root of a binary tree, imagine yourself standing on the right side of it, return the values of the nodes you can see ordered from top to bottom.
+
+#
+class Solution:
+    def invertTree(self, root):
+
+        return
+
+# Score Card
+# Did I need hints? Y for the optimal solution my original wasn't really bad
+# Did you finish within 30 min? N
+# Was the solution optimal? Yes
+# Were there any bugs? See note above the method with heaps
+#  3 3 5 4 = 3.75
