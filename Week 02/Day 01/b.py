e2e4716fc9f5a6a1ab46962a3a960fd9eb7e0c9f
Kevin Knott <KKnoTT93@gmail.com>
2021-06-08 13:17:05 -0500

    Refactoring so that we have better structure and adding 3 problems for day 8

---
+++ Week 02/Day 01/b.py
@@ -0,0 +1,16 @@
+#  Count Binary Substrings: https://leetcode.com/problems/count-binary-substrings/
+
+# Give a binary string s, return the number of non-empty substrings that have the same number of 0's and 1's, and all the 0's and all the 1's in these substrings are grouped consecutively.
+# Substrings that occur multiple times are counted the number of times they occur.
+
+
+class Solution:
+    def countBinarySubstrings(self, s: str) -> int:
+        return
+
+# Score Card
+# Did I need hints? N
+# Did you finish within 30 min? Y
+# Was the solution optimal? Y
+# Were there any bugs? Y  (no support for max heaps made me use negatives and I forgot to handle them properly)
+#  5 5 5 3 = 4.5
