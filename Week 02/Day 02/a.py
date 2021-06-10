9f475936f5e22936e7a5fa83dee6b56689542192
Kevin Knott <KKnoTT93@gmail.com>
2021-06-09 21:05:27 -0500

    Decoding ways round 2

--- Week 02/Day 02/a.py
+++ Week 02/Day 02/a.py
@@ -18,12 +18,63 @@
 # The answer is guaranteed to fit in a 32-bit integer.
 
 
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        def dfs(index=0):
+            # Base Case
+            if index == len(s):
+                return 1
+
+            if s[index] == '0':
+                return 0
+
+            if index == len(s) - 1:
+                return 1
+
+            # I accidently added a bug my initial though for making this was with a for loop
+            # however that basically makes this run indefinitely so I had to change it to a
+            # recurse which I should of known in the first place
+            count = dfs(index + 1)
+            # Otherwise go down by two
+            if int(s[index:index+2]) <= 26:
+                count += dfs(index+2)
+
+            return count
+
+        return dfs()
+
+    # Can we improve on this? Yes!
+    # We can memoize!
+
+    def numDecodingsMemo(self, s: str) -> int:
+        def dfs(index=0):
+            # Base Case
+            if index == len(s):
+                return 1
+
+            if s[index] == '0':
+                return 0
+
+            if index == len(s) - 1:
+                return 1
+
+            # Since we are actually going down 1 at a time first and then by 2s
+            # we are actually repeating work
+            count = dfs(index + 1)
+            # Otherwise go down by two
+            if int(s[index:index+2]) <= 26:
+                count += dfs(index+2)
+
+            self.memo[index] = count
+            return count
+
+        self.memo = {}
+        return dfs()
+
+
 # Score Card
-# Did I need hints? N
+# Did I need hints? Y
 # Did you finish within 30 min? Y
-# Was the solution optimal? Unkown
-# Were there any bugs? Y  (no support for max heaps made me use negatives and I forgot to handle them properly)
-
-
-#  To improve this code I think that i need to separate out the pieces that parse atoms and parse the amount of atoms
-#  4 1 1 3 = 2.25
+# Was the solution optimal? Pretty well although there is a dynamic programming solution
+# Were there any bugs? My base cases need to be in a certain order and what I mentioned in the problem
+#  3 3 1 3 = 2.5
