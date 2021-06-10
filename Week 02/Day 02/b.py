bf209dcea78eb51f6b96040b4f77397b4d6af848
Kevin Knott <KKnoTT93@gmail.com>
2021-06-09 21:35:38 -0500

    Adding the score

--- Week 02/Day 02/b.py
+++ Week 02/Day 02/b.py
@@ -57,4 +57,4 @@ class Solution:
 # Did you finish within 30 min? Y
 # Was the solution optimal? Y
 # Were there any bugs? Y  (no support for max heaps made me use negatives and I forgot to handle them properly)
-#  2 1 3 4 = 2.5
+#  5 5 3 5 = 4.5
