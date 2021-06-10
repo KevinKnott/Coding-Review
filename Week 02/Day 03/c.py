533cf524bfc78e9dd4ce7e27cc2842cfc7fd6a90
Kevin Knott <KKnott93@gmail.com>
2021-06-10 17:28:47 -0500

    Adding problems

---
+++ Week 02/Day 03/c.py
@@ -0,0 +1,16 @@
+# Best Time to Buy and Sell Stock: https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+
+# You are given an array prices where prices[i] is the price of a given stock on the ith day.
+# You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+# Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        return
+
+# Score Card
+# Did I need hints? Ish
+# Did you finish within 30 min? N
+# Was the solution optimal? The result ends up parsing in O(N) but it is divide and conquer
+# Were there any bugs? So many edge cases for english
+#  2 1 4 2 = 2.25
