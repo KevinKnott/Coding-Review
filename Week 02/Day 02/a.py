3c194999fa0a6a2a8381ecd4fa66096a67909a82
Kevin Knott <KKnoTT93@gmail.com>
2021-06-09 21:14:32 -0500

    Adding dp solution (I didn't write it perfectly but copied from solution)

--- Week 02/Day 02/a.py
+++ Week 02/Day 02/a.py
@@ -71,10 +71,32 @@ class Solution:
         self.memo = {}
         return dfs()
 
+    # The dynamic programming solution is almost identical to above except you check how many you had before
+    # instead of adding to the stacked value
+    def numDecodingsDP(self, s: str) -> int:
+        # The array has an extra spot because creating solutions that start with a len of 2
+        dp = [0] * (len(s) + 1)
+        # Create base case where if s[0] is not 0 we have at least one possible so far
+        dp[0] = 1
+        dp[1] = 1 if s[0] != '0' else 0
+
+        for i in range(2, len(dp)):
+
+            # Check 1
+            if s[i-1] != '0':
+                dp[i] = dp[i-1]
+
+            # Check 2
+            temp = int(s[i-2: i])
+            if 10 <= temp and temp <= 26:
+                dp[i] += dp[i-2]
+
+        return dp[len(s)]
 
 # Score Card
 # Did I need hints? Y
 # Did you finish within 30 min? Y
-# Was the solution optimal? Pretty well although there is a dynamic programming solution
+# Was the solution optimal? Pretty well although there is a dynamic programming solution and there is even a o(n) with two pointers
+# This actually kind of reminds of finding the fibonaci where it is based off the two previous answers
 # Were there any bugs? My base cases need to be in a certain order and what I mentioned in the problem
 #  3 3 1 3 = 2.5
