ca4780b236640c0ad813ce47d47a23a15b4ccfb3
Kevin Knott <KKnoTT93@gmail.com>
2021-06-11 13:44:32 -0500

    Super easy min stack problem

--- Week 02/Day 04/b.py
+++ Week 02/Day 04/b.py
@@ -10,24 +10,35 @@
 #     int getMin() retrieves the minimum element in the stack.
 
 
+# This is actually super easy you just have to understand that on a stack you can have pairs
+# my solution technically doesn't check for empty stacks but in production you would want to
+# have the commented out checks or raising an exception
 class MinStack:
 
     def __init__(self):
         """
         initialize your data structure here.
         """
+        self.stack = []
 
     def push(self, val: int) -> None:
+        if len(self.stack) == 0:
+            self.stack.append((val, val))
+        else:
+            self.stack.append((val, min(val, self.getMin())))
 
     def pop(self) -> None:
+        return self.stack.pop()[0]  # if len(self.stack) != 0 else None
 
     def top(self) -> int:
+        return self.stack[-1][0]  # if len(self.stack) != 0 else None
 
     def getMin(self) -> int:
+        return self.stack[-1][1]  # if len(self.stack) != 0 else None
 
-        # Score Card
-        # Did I need hints? N (But the second solution did)
-        # Did you finish within 30 min? No 1:30
-        # Was the solution optimal? My initial solution is optimal however I messed up the initial coding of it
-        # Were there any bugs? I forgot that since it is possible to have [[[[]]]] I need to actually recurse
-        #  4 1 2 1 = 2
+    # Score Card
+    # Did I need hints? N
+    # Did you finish within 30 min? Y 6 min
+    # Was the solution optimal? Y
+    # Were there any bugs? N
+    #  5 5 5 5 = 5
