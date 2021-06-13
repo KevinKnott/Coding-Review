9eff039f42c090d4750d74e32fad6ad1a298b29f
Kevin Knott <KKnott93@gmail.com>
2021-06-13 17:51:00 -0500

    Setup problems 1-4

---
+++ Week 02/Day 06/b.py
@@ -0,0 +1,14 @@
+# Top K Frequent Elements: https://leetcode.com/problems/top-k-frequent-elements/
+# Given an integer array nums and an integer k, return the k most frequent elements. You may return the answer in any order.
+
+class Solution:
+    def topKFrequent(self, nums, k):
+        return
+
+
+# Score Card
+# Did I need hints? N
+# Did you finish within 30 min? Y 25
+# Was the solution optimal? Y
+# Were there any bugs? N
+#  5 5 3 5 = 4.5
---
+++ Week 02/Day 06/c.py
@@ -0,0 +1,14 @@
+# Valid Parentheses: https://leetcode.com/problems/valid-parentheses/
+# Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+class Solution:
+    def isValid(self, s):
+        return
+
+
+# Score Card
+# Did I need hints? N
+# Did you finish within 30 min? Y
+# Was the solution optimal? My initial solution is optimal
+# Were there any bugs? None
+#  5 5 5 5 = 5
