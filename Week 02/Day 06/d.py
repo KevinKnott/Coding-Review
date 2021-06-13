b315e67a7e779f48b96c3d3cf1262b220fc090db
Kevin Knott <KKnott93@gmail.com>
2021-06-13 17:51:00 -0500

    Setup problems 1-4

---
+++ Week 02/Day 06/d.py
@@ -0,0 +1,15 @@
+# Palindrome Linked List: https://leetcode.com/problems/palindrome-linked-list/
+# Given the head of a singly linked list, return true if it is a palindrome.
+
+class Solution:
+    def isValid(self, s):
+
+        return
+
+# The answer is guaranteed to fit in a 32-bit integer.
+# Score Card
+# Did I need hints? N
+# Did you finish within 30 min? N
+# Was the solution optimal? It is optimal enough however I think I could make some slight improvements
+# Were there any bugs? I am having issues with double negative values
+# 5 1 3 3 = 3
