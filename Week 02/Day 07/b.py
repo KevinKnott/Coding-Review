a40e8105b015dfc8e57c28fa1ef727ad568c44aa
Kevin Knott <KKnoTT93@gmail.com>
2021-06-14 22:10:39 -0500

    Topologic sort I need to review this in CLRS or my other book as it is a bit rough

--- Week 02/Day 07/b.py
+++ Week 02/Day 07/b.py
@@ -4,10 +4,57 @@
 #     For example, the pair [0, 1], indicates that to take course 0 you have to first take course 1.
 # Return the ordering of courses you should take to finish all courses. If there are many valid answers, return any of them. If it is impossible to finish all courses, return an empty array.
 
+# I know that this problem is a topological sort however I haven't coded one in  while
+# The basic premise is to build a dfs where you visit all courses with a model of White (Unvisited) Grey (In Calc) and Black (Visited with no reqs)
+
 
 class Solution:
-    def findOrder(self, numCourses, prerequisites):
-        return
+    WHITE = 1
+    GRAY = 2
+    BLACK = 3
+
+    # O(V + E) we create a graph of vertex to edge and visit them all only once
+    # o(V+E) since we have a ajdacency list of V and a stack of potentially E
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        graph = {}
+        color = {course: Solution.WHITE for course in range(numCourses)}
+        topologicalOrder = []
+        isPossible = True
+
+        for dest, src in prerequisites:
+            if src not in graph:
+                graph[src] = []
+            graph[src].append(dest)
+
+        def topologicalSort(course):
+            nonlocal isPossible
+
+            if not isPossible:
+                return False
+
+            color[course] = Solution.GRAY
+            if course in graph:
+                for i in graph[course]:
+                    if color[i] == Solution.WHITE:
+                        topologicalSort(i)
+                    elif color[i] == Solution.GRAY:
+                        isPossible = False
+                        return
+
+            color[course] = Solution.BLACK
+            topologicalOrder.append(course)
+
+            return
+
+        for course in range(numCourses):
+            if color[course] == Solution.WHITE:
+                topologicalSort(course)
+
+        return topologicalOrder[::-1] if isPossible else []
+
+# With a little bit of help I have been able to reproduce the algorithm
+# But is there a more optimal way? I am not sure but I will have to look it up
+
 
 # Score Card
 # Did I need hints? N
