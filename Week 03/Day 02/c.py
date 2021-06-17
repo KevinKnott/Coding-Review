1e64501a66013e68001ab7963b0c029767658cb6
Kevin Knott <KKnoTT93@gmail.com>
2021-06-16 20:35:51 -0500

    Adding initial problem definition

---
+++ Week 03/Day 02/c.py
@@ -0,0 +1,18 @@
+# Merge Two Sorted Lists: https://leetcode.com/problems/merge-two-sorted-lists/
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        return
+# Score Card
+# Did I need hints? Ish
+# Did you finish within 30 min? N
+# Was the solution optimal? The result ends up parsing in O(N) but it is divide and conquer
+# Were there any bugs? So many edge cases for english
+#  2 1 4 2 = 2.25
