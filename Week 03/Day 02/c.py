b0ed4bef240b52145bfe5fbb8e3df5ac270969f5
Kevin Knott <KKnoTT93@gmail.com>
2021-06-16 22:34:18 -0500

    Easy problem that I actually know really well

--- Week 03/Day 02/c.py
+++ Week 03/Day 02/c.py
@@ -1,5 +1,7 @@
 # Merge Two Sorted Lists: https://leetcode.com/problems/merge-two-sorted-lists/
 
+# Merge two sorted linked lists and return it as a sorted list. The list should be made by splicing together the nodes of the first two lists.
+
 # Definition for singly-linked list.
 class ListNode:
     def __init__(self, val=0, next=None):
@@ -7,12 +9,34 @@ class ListNode:
         self.next = next
 
 
+# my initial thoughts for this problem is actually to do exactly as it says
+# while i have both nodes compare otherwise append whatever is left
+
 class Solution:
     def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
-        return
+        result = ListNode(0)
+        cur = result
+
+        while l1 and l2:
+            if l1.val <= l2.val:
+                cur.next = l1
+                l1 = l1.next
+            else:
+                cur.next = l2
+                l2 = l2.next
+            cur = cur.next
+
+        if l1:
+            cur.next = l1
+
+        if l2:
+            cur.next = l2
+
+        return result.next
+
 # Score Card
-# Did I need hints? Ish
-# Did you finish within 30 min? N
-# Was the solution optimal? The result ends up parsing in O(N) but it is divide and conquer
-# Were there any bugs? So many edge cases for english
-#  2 1 4 2 = 2.25
+# Did I need hints? Nope
+# Did you finish within 30 min? 4 minutes
+# Was the solution optimal? Yup this runs in o(n+m) time in worst case and uses o(1) space
+# Were there any bugs? None
+# 5 5 5 5 = 5
