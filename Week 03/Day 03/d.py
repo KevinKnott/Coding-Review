de0b9fcc0dff145d7db1fceb5f5a6ac8496ea8d6
Kevin Knott <KKnoTT93@gmail.com>
2021-06-17 20:11:24 -0500

    Setting up the problems for day 17

---
+++ Week 03/Day 03/d.py
@@ -0,0 +1,16 @@
+#  Add Strings: https://leetcode.com/problems/add-strings/
+
+# Given two non-negative integers, num1 and num2 represented as string, return the sum of num1 and num2 as a string.
+# You must solve the problem without using any built-in library for handling large integers (such as BigInteger). You must also not convert the inputs to integers directly.
+
+
+class Solution:
+    def addStrings(self, num1: str, num2: str) -> str:
+
+        return
+# Score Card
+# Did I need hints? N
+# Did you finish within 30 min? 10
+# Was the solution optimal? My initial is optimal for time but not space and the second one is fully optimal
+# Were there any bugs? I forgot that I need to create a new node if the other two nodes didn't exist and we have a carry over
+#  5 5 5 4 = 4.75
