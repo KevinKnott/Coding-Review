d79fa8e4f1ebe9eee6daa02476c012f33cd8fbd8
Kevin Knott <KKnoTT93@gmail.com>
2021-06-18 18:21:06 -0500

    Adding problems for the day

---
+++ Week 03/Day 04/a.py
@@ -0,0 +1,16 @@
+# Product of Array Except Self: https://leetcode.com/problems/product-of-array-except-self/
+
+# Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
+# The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+# You must write an algorithm that runs in O(n) time and without using the division operation.
+
+class Solution:
+    def productExceptSelf(self, nums):
+        return
+
+# Score Card
+# Did I need hints? N
+# Did you finish within 30 min? Y
+# Was the solution optimal? Y
+# Were there any bugs? Forgot to return results
+#  5 5 5 4 = 4.75
---
+++ Week 03/Day 04/b.py
@@ -0,0 +1,16 @@
+# Minimum Window Substring: https://leetcode.com/problems/minimum-window-substring/
+
+# Given two strings s and t of lengths m and n respectively, return the minimum window substring of s such that every character in t (including duplicates) is included in the window. If there is no such substring, return the empty string "".
+# The testcases will be generated such that the answer is unique.
+# A substring is a contiguous sequence of characters within the string.
+
+class Solution:
+    def minWindow(self, s, t):
+        return
+
+# Score Card
+# Did I need hints? N
+# Did you finish within 30 min? Y 6 min
+# Was the solution optimal? Y
+# Were there any bugs? N
+#  5 5 5 5 = 5
