8510350327dc2499ef71167e184b8bb0fa1707d2
Kevin Knott <KKnoTT93@gmail.com>
2021-06-18 19:43:03 -0500

    Simple problem one

--- Week 03/Day 04/a.py
+++ Week 03/Day 04/a.py
@@ -4,13 +4,28 @@
 # The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
 # You must write an algorithm that runs in O(n) time and without using the division operation.
 
+# What we can do is create a prefix/suffix array and loop through and multiply everything together minus where you are at
+
 class Solution:
     def productExceptSelf(self, nums):
-        return
+        prefix = [1] * (len(nums))
+        postfix = [1] * (len(nums))
+
+        for i in range(1, len(nums)):
+            prefix[i] = prefix[i-1] * nums[i-1]
+
+        for i in range((len(nums)-2), -1, -1):
+            postfix[i] = postfix[i+1] * nums[i+1]
+
+        return [prefix[i] * postfix[i] for i in range(len(nums))]
+
+# Can we possibly do better than the above o(N) time and space solution from above?
+# not really I mean if we mutate the array and multiply across and then run through the second loop
+# using a multiplication sum we could probably do it but that would end up mutating the original array/list
 
 # Score Card
 # Did I need hints? N
 # Did you finish within 30 min? Y
 # Was the solution optimal? Y
-# Were there any bugs? Forgot to return results
-#  5 5 5 4 = 4.75
+# Were there any bugs? N
+#  4 4 5 5 = 4.5
