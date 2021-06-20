7917c93679dcc1908077b3ba5f583e8637d0c1a5
Kevin Knott <KKnoTT93@gmail.com>
2021-06-20 00:02:22 -0500

    Final problem was a simple reversing of a linked list with some extra parameters

--- Week 03/Day 05/d.py
+++ Week 03/Day 05/d.py
@@ -9,14 +9,44 @@ class ListNode:
         self.next = next
 
 
+# Reversing is simple enough however we need to adapt the algorithm to handle a variable range based off of the values instead
+# There is one edge case that I can think of where the head is the start of the reversing and two
+
 class Solution:
     def reverseBetween(self, head, left, right):
-        return
+        if not head:
+            return None
+
+        # Return if there is nothing to reverse
+        if left == right:
+            return head
+
+        dummy = ListNode(0)
+        dummy.next = head
+        pre = dummy
+
+        # Traverse across the linked list until you find the reversing place
+        for i in range(left - 1):
+            pre = pre.next
+
+        # Set previous to be None or whatever was before
+        prev = None
+        cur = pre.next
+        for i in range(right - left + 1):
+            next = cur.next
+            cur.next = prev
+            prev = cur
+            cur = next
+
+        pre.next.next = cur
+        pre.next = prev
+
+        return dummy.next
 
 
 # Score Card
 # Did I need hints? N
-# Did you finish within 30 min? N
-# Was the solution optimal? It is optimal enough however I think I could make some slight improvements
-# Were there any bugs? I am having issues with double negative values
-# 5 1 3 3 = 3
+# Did you finish within 30 min? Y
+# Was the solution optimal? I believe that it is optimal as it runs in o(n) time and doesn't really use any space
+# Were there any bugs? It took me a second to realize that left and right were indexes to reverse at and  not values
+# 3 3 3 3 = 3
