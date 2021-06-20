aa6a492b7f4e7e09351c6ee839cd132a82f4d258
Kevin Knott <KKnoTT93@gmail.com>
2021-06-20 15:32:18 -0500

    Adding problems (and solving the first problem)

---
+++ Week 03/Day 06/d.py
@@ -0,0 +1,17 @@
+# Remove All Adjacent Duplicates in String II: https://leetcode.com/problems/remove-all-adjacent-duplicates-in-string-ii/
+
+# You are given a string s and an integer k, a k duplicate removal consists of choosing k adjacent and equal letters from s and removing them, causing the left and the right side of the deleted substring to concatenate together.
+# We repeatedly make k duplicate removals on s until we no longer can.
+# Return the final string after all such duplicate removals have been made. It is guaranteed that the answer is unique.
+
+class Solution:
+    def removeDuplicates(self, s, k):
+        return
+
+
+# Score Card
+# Did I need hints? N
+# Did you finish within 30 min? N
+# Was the solution optimal? It is optimal enough however I think I could make some slight improvements
+# Were there any bugs? I am having issues with double negative values
+# 5 5 5 3 = 4.5
