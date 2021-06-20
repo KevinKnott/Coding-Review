b645ff48a61468ebc8d69435deab55bd0914414a
Kevin Knott <KKnoTT93@gmail.com>
2021-06-20 17:48:52 -0500

    Done for the day

--- Week 03/Day 06/d.py
+++ Week 03/Day 06/d.py
@@ -4,14 +4,76 @@
 # We repeatedly make k duplicate removals on s until we no longer can.
 # Return the final string after all such duplicate removals have been made. It is guaranteed that the answer is unique.
 
+# So if we take a string right and we use a stack we can count how many of the same letters we have in a row and if we end up with k in a row we can pop k - 1 off stack
+# and then continue through the problem however this won't work if we don't know the k value  so we can use that on the stack as well
+# Aka this problem is like having the min stack where you return the min value up to a point
+
 class Solution:
     def removeDuplicates(self, s, k):
-        return
+        if len(s) <= 1:
+            return s
+
+        stack = []
+
+        for char in s:
+            if len(stack) > 0:
+                tempChar, count = stack[-1]
+
+                if char == tempChar:
+                    if count == k - 1:
+                        for _ in range(k-1):
+                            stack.pop()
+                    else:
+                        stack.append((char, count+1))
+                else:
+                    stack.append((char, 1))
+            else:
+                stack.append((char, 1))
+
+        # unpack the stack that we created!
+        result = ''
+        for x in stack:
+            result += x[0]
+        return result
+
+# This works but is it optimized? This will in worst case run through o(n) time and space can we do better
+# I think it is possible to improve this by keeping track of indicies instead of counts but I am unsure.
+
+# after looking at the solution it appears there is a slightly better algo that can reduce the space complexity
+# by using two pointers and a stack you use the two pointers to compare the values and the stack to keep counts
+# at the end you simply return the same string except you cut it at length of the slow pointer
+    def removeDuplicatesOptimized(self, s, k):
+        if k > len(s):
+            return s
+        stack = []
+        j = 0
+        result = list(s)
+
+        for i in range(len(s)):
+            # Make the value at fast pointer equal the slow pointer (we are modifying as we go)
+            result[j] = result[i]
+            # If we have a new char append the count to stack
+            if j == 0 or result[j] != result[j - 1]:
+                stack.append(1)
+            else:
+                # Check if the char is equal to k or increment by one
+                count = stack.pop() + 1
+                if count == k:
+                    j = j - k
+                else:
+                    stack.append(count)
+
+            j += 1
+
+        temp = ''
+        for i in range(j):
+            temp += result[i]
+        return temp
 
 
 # Score Card
-# Did I need hints? N
-# Did you finish within 30 min? N
-# Was the solution optimal? It is optimal enough however I think I could make some slight improvements
-# Were there any bugs? I am having issues with double negative values
-# 5 5 5 3 = 4.5
+# Did I need hints? Y
+# Did you finish within 30 min? Y
+# Was the solution optimal? There was a interesting solution that had better space complexity but is more complicated
+# Were there any bugs? Nope this was pretty straight forward
+# 3 4 4 5 = 4
