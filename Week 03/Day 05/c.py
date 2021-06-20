623a57421d8f3811c460fac7f2e46a99a6bf3379
Kevin Knott <KKnoTT93@gmail.com>
2021-06-19 22:28:09 -0500

    Adding problems for the day

---
+++ Week 03/Day 05/c.py
@@ -0,0 +1,16 @@
+#  Subdomain Visit Count: https://leetcode.com/problems/subdomain-visit-count/
+
+# A website domain like "discuss.leetcode.com" consists of various subdomains. At the top level, we have "com", at the next level, we have "leetcode.com", and at the lowest level, "discuss.leetcode.com". When we visit a domain like "discuss.leetcode.com", we will also visit the parent domains "leetcode.com" and "com" implicitly.
+# Now, call a "count-paired domain" to be a count (representing the number of visits this domain received), followed by a space, followed by the address. An example of a count-paired domain might be "9001 discuss.leetcode.com".
+# We are given a list cpdomains of count-paired domains. We would like a list of count-paired domains, (in the same format as the input, and in any order), that explicitly counts the number of visits to each subdomain.
+
+class Solution:
+    def subdomainVisits(self, cpdomains):
+        return
+
+# Score Card
+# Did I need hints? N
+# Did you finish within 30 min? Y
+# Was the solution optimal? My initial solution is optimal
+# Were there any bugs? None
+#  5 5 5 5 = 5
