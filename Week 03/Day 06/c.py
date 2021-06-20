b9ea3537ffca456097f34322eae5bb7c44687352
Kevin Knott <KKnoTT93@gmail.com>
2021-06-20 15:32:18 -0500

    Adding problems (and solving the first problem)

---
+++ Week 03/Day 06/c.py
@@ -0,0 +1,16 @@
+# Group Anagrams: https://leetcode.com/problems/group-anagrams/
+
+# Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+# An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+class Solution:
+    def groupAnagrams(self, strs):
+        return
+
+
+# Score Card
+# Did I need hints? N
+# Did you finish within 30 min? Y
+# Was the solution optimal? Oh yea
+# Were there any bugs? The bug I mentioned above
+#  5 5 5 3 = 4.5
