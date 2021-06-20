052242c706fd9ad7d615e71fd6a122ae585d3097
Kevin Knott <KKnoTT93@gmail.com>
2021-06-19 22:28:09 -0500

    Adding problems for the day

---
+++ Week 03/Day 05/b.py
@@ -0,0 +1,14 @@
+# Integer to English Words: https://leetcode.com/problems/integer-to-english-words/
+
+# Convert a non-negative integer num to its English words representation.
+
+class Solution:
+    def numberToWords(self, num: int) -> str:
+        return
+
+# Score Card
+# Did I need hints? N
+# Did you finish within 30 min? Y 25
+# Was the solution optimal? Y
+# Were there any bugs? N
+#  5 5 3 5 = 4.5
