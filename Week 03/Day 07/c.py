2b4c7996551190a529d3800bb2e32480868e6be8
Kevin Knott <KKnott93@gmail.com>
2021-06-21 23:25:22 -0500

    Another divide and conquer kind of day

--- Week 03/Day 07/c.py
+++ Week 03/Day 07/c.py
@@ -3,14 +3,60 @@
 # Given an integer array nums and an integer k, return the kth largest element in the array.
 # Note that it is the kth largest element in the sorted order, not the kth distinct element.
 
-class Solution:
-    def findKthLargest(self, nums, k):
-        return
+# There are a number of simple solutions to this problem but it is all about efficency
+# We can sort the array and return len(nums) - k in an nlogn time
+# we could use a heap to grab the max value every time and pop k -1 times and return result as pop() this is nlogk time
+# or we could actually use the quick sort method to binary search and potentially grab the correct spot n time (wehere we chose the wrong n everytime)
 
+import heapq
+from random import randint
+
+
+class Solution:
+    def findKthLargestSort(self, nums, k):
+        nums.sort()
+        return nums[len(nums)-k]
+
+    def findKthLargestHeap(self, nums, k):
+        heapq.heapify(nums)
+        # Get the n largest values and return the top of it
+        return heapq.nlargest(k, nums)[-1]
+
+    def findKthLargestQuickSort(self, nums, k):
+        def conquer(start, end):
+            pivot = randint(start, end)
+            nums[pivot], nums[end] = nums[end], nums[pivot]
+            pivot = end
+
+            swap = start
+            for i in range(start, end):
+                if nums[i] < nums[pivot]:
+                    nums[i], nums[swap] = nums[swap], nums[i]
+                    swap += 1
+
+            nums[swap], nums[pivot] = nums[pivot], nums[swap]
+
+            return swap
+
+        def divide(target, start, end):
+            # I forgot if start == end there is only one val so we dont need to do anything
+            if start == end:
+                return nums[start]
+
+            pivot = conquer(start, end)
+
+            if pivot < target:
+                return divide(target, pivot + 1, end)
+            elif pivot > target:
+                return divide(target, start, pivot - 1)
+            else:
+                return nums[pivot]
+
+        return divide(len(nums) - k, 0, len(nums)-1)
 
 # Score Card
-# Did I need hints? Y
-# Did you finish within 30 min? Noooooo oh god
-# Was the solution optimal? Oh yea however I needed lots of hints although the backtracking was actually not the hard part
-# Were there any bugs? I initially messed up passing in the next row/col by not increasing row when overflowing
-#  1 1 3 3 = 2
+# Did I need hints? N
+# Did you finish within 30 min? Yuh
+# Was the solution optimal? Yeah it is
+# Were there any bugs? I forgot to return from my recursion
+# 5 5 5 4 = 4.75
