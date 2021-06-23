c49a01a460835b575fe4a0e19d3da7a2de8cc065
Kevin Knott <KKnoTT93@gmail.com>
2021-06-22 19:35:52 -0500

    Adding problems for the new day in a new week of reviewing

---
+++ Week 04/Day 01/a.py
@@ -0,0 +1,16 @@
+# Minimum Window Substring: https://leetcode.com/problems/minimum-window-substring/
+
+# Given two strings s and t of lengths m and n respectively, return the minimum window substring of s such that every character in t (including duplicates) is included in the window. If there is no such substring, return the empty string "".
+# The testcases will be generated such that the answer is unique.
+# A substring is a contiguous sequence of characters within the string.
+
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        return
+
+# Score Card
+# Did I need hints? N
+# Did you finish within 30 min? N 50 or so
+# Was the solution optimal? Y
+# Were there any bugs? Y (I forgot about handling leading 0 since I was using if value and value could be 0)
+#  4 1 4 2 = 2.75
---
+++ Week 04/Day 01/b.py
@@ -0,0 +1,15 @@
+# Kth Smallest Element in a Sorted Matrix: https://leetcode.com/problems/kth-smallest-element-in-a-sorted-matrix/
+
+# Given an n x n matrix where each of the rows and columns are sorted in ascending order, return the kth smallest element in the matrix.
+# Note that it is the kth smallest element in the sorted order, not the kth distinct element.
+
+class Solution:
+    def kthSmallest(self, matrix, k):
+        return
+
+# Score Card
+# Did I need hints? N
+# Did you finish within 30 min? Y
+# Was the solution optimal? Kind of we could make some slight improvements to help speed they are listed above
+# Were there any bugs? N
+#  5 5 3 5 = 4.5
