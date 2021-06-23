31b586bcc3ebc352eae061fbdb96a66999a10288
Kevin Knott <KKnoTT93@gmail.com>
2021-06-22 19:35:52 -0500

    Adding problems for the new day in a new week of reviewing

---
+++ Week 04/Day 01/c.py
@@ -0,0 +1,13 @@
+#  Subarray Sum Equals K: https://leetcode.com/problems/subarray-sum-equals-k/
+#  Given an array of integers nums and an integer k, return the total number of continuous subarrays whose sum equals to k.
+
+class Solution:
+    def subarraySum(self, nums, k):
+        return
+
+# Score Card
+# Did I need hints? Y
+# Did you finish within 30 min? Y
+# Was the solution optimal? Y
+# Were there any bugs? N
+#  1 3 2 5 = 2.75
