68506c71a7357513e88eff27a514b1c340b0d81f
Kevin Knott <KKnoTT93@gmail.com>
2021-06-22 19:35:52 -0500

    Adding problems for the new day in a new week of reviewing

---
+++ Week 04/Day 01/d.py
@@ -0,0 +1,30 @@
+#  Moving Average from Data Stream: https://leetcode.com/problems/moving-average-from-data-stream/
+
+# Given a stream of integers and a window size, calculate the moving average of all integers in the sliding window.
+# Implement the MovingAverage class:
+
+#     MovingAverage(int size) Initializes the object with the size of the window size.
+#     double next(int val) Returns the moving average of the last size values of the stream.
+
+class MovingAverage:
+
+    def __init__(self, size: int):
+        """
+        Initialize your data structure here.
+        """
+        return
+
+    def next(self, val: int) -> float:
+        return
+
+
+# Your MovingAverage object will be instantiated and called as such:
+# obj = MovingAverage(size)
+# param_1 = obj.next(val)
+
+# Score Card
+# Did I need hints? N
+# Did you finish within 30 min? Y
+# Was the solution optimal? Y although you can read my blurb above about a potentially better solution it is rather complicated
+# Were there any bugs? N
+#  5 5 3 5 = 4.5
