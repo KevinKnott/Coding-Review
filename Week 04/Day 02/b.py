880a732f5501319c0c6fafec957422cdf0a50480
Kevin Knott <KKnoTT93@gmail.com>
2021-06-23 19:30:16 -0500

    Problems added for the day

--- Week 04/Day 02/b.py
+++ Week 04/Day 02/b.py
@@ -1,4 +1,17 @@
+# Symmetric Tree: https://leetcode.com/problems/symmetric-tree/
 
+# Given the root of a binary tree, check whether it is a mirror of itself (i.e., symmetric around its center).
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def isSymmetric(self, root):
+        return
 
 # Score Card
 # Did I need hints? Y
--- Week 04/Day 02/d.py
+++ Week 04/Day 02/d.py
@@ -1,3 +1,21 @@
+#  Lowest Common Ancestor of a Binary Tree: https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/
+
+# Given a binary tree, find the lowest common ancestor (LCA) of two given nodes in the tree.
+
+# According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between two nodes p and q as the lowest node in T that has both p and q as descendants (where we allow a node to be a descendant of itself).”
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def lowestCommonAncestor(self, root, p, q):
+        return
 
 # Score Card
 # Did I need hints? N
