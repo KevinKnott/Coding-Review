9e56e533425be7f0bd4274df7d1d8b388f98c977
Kevin Knott <KKnoTT93@gmail.com>
2021-06-25 17:02:28 -0500

    Adding files for problems

---
+++ Week 04/Day_04/b.py
@@ -0,0 +1,8 @@
+
+
+# Score Card
+# Did I need hints? Y
+# Did you finish within 30 min? Y
+# Was the solution optimal? Y although I messed up the thought process of how to solve the problem
+# Were there any bugs?  I didn't really have any bugs
+#  2 3 3 5 = 3.25
