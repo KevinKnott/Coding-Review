3dadfb84336226d69f60c158158d5a3b686599bd
Kevin Knott <KKnoTT93@gmail.com>
2021-06-24 20:54:26 -0500

    Adding the problems for the day

---
+++ Week 04/Day 03/d.py
@@ -0,0 +1,14 @@
+# Pow(x, n): https://leetcode.com/problems/powx-n/
+
+# Implement pow(x, n), which calculates x raised to the power n (i.e., xn).
+
+class Solution:
+    def myPow(self, x, n):
+        return
+
+# Score Card
+# Did I need hints? N
+# Did you finish within 30 min? 10
+# Was the solution optimal? This is optimal
+# Were there any bugs? No
+#  5 5 5 5 = 5
