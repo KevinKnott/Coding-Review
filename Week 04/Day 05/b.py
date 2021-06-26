cb9c25104386eeb696629a1a509464576e735664
Kevin Knott <KKnott93@gmail.com>
2021-06-26 17:53:55 -0500

    Cheeky max path problem that isn't actually too hard

--- Week 04/Day 05/b.py
+++ Week 04/Day 05/b.py
@@ -12,14 +12,41 @@ class TreeNode:
         self.right = right
 
 
+# So my initial intuition was to build a dfs straight down and it seems to work for the most part
+# that being said the code is laking an overall best that is stored outside of our dfs
+# this means that if ta sub tree has the most optimal path we will keep computing and ignore it
+
 class Solution:
     def maxPathSum(self, root: TreeNode) -> int:
-        return
+        def dfs(node):
+            if not node:
+                return 0
 
+            # So if this is negative we can just ignore it as we want max path
+            left = max(dfs(node.left), 0)
+            right = max(dfs(node.right), 0)
+
+            # update our result if the tree created from root the left and right is larger
+            # our current result
+            self.maxSum = max(self.maxSum, left + node.val + right)
+
+            # otherwise we need to return the max path of left /right plus our nodfe
+            return node.val + max(left, right)
+
+        self.maxSum = -float('inf')
+        dfs(root)
+
+        return self.maxSum
+
+# The above is optimal and runs in o(n) time and o(1) space the tricky part is actually determining
+# which paths weneed to follow in this case we had to get the max result at every node of it
+# and its paths to the left or right and the return the max of the biggest path up to that point
+# it took me a minute to figure it out
 
 # Score Card
-# Did I need hints? Y
-# Did you finish within 30 min? Y
-# Was the solution optimal? Y although I messed up the thought process of how to solve the problem
-# Were there any bugs?  I didn't really have any bugs
-#  2 3 3 5 = 3.25
+# Did I need hints? N
+# Did you finish within 30 min? Y 20 min
+# Was the solution optimal? Y
+# Were there any bugs?  I had one bug from when I was returning a complete path back up instead of the mac
+# of the left or the right
+# 5 5 5 4 = 4.75
