8c2f2ca874dfc2e7430626ea833a5e647818b712
Kevin Knott <KKnott93@gmail.com>
2021-06-26 17:18:54 -0500

    Daily problems lezzgo

---
+++ Week 04/Day 05/d.py
@@ -0,0 +1,15 @@
+# Shortest Subarray with Sum at Least K: https://leetcode.com/problems/shortest-subarray-with-sum-at-least-k/
+
+# Return the length of the shortest, non-empty, contiguous subarray of nums with sum at least k.
+# If there is no non-empty subarray with sum at least k, return -1.
+
+class Solution:
+    def shortestSubarray(self, nums, k: int) -> int:
+        return
+
+# Score Card
+# Did I need hints? N
+# Did you finish within 30 min? 10
+# Was the solution optimal? This is optimal
+# Were there any bugs? No
+#  5 5 5 5 = 5
