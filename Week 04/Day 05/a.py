653192b78532e9ee43caa5d8946dcb02eb9f65d9
Kevin Knott <KKnott93@gmail.com>
2021-06-26 17:32:06 -0500

    Forgot to add my score

--- Week 04/Day 05/a.py
+++ Week 04/Day 05/a.py
@@ -40,8 +40,7 @@ class Solution:
 
 # Score Card
 # Did I need hints? N
-# Did you finish within 30 min? N (45 or so)
-# Was the solution optimal? I believe so we could make some slight optimization but this will run in o(n^2) because of the multiplicity we would go through once and then again to multiply
-#  and o(n) space
-# Were there any bugs? I listed bugs in the above code
-#  5 2 4 2 = 3.25
+# Did you finish within 30 min? 10
+# Was the solution optimal? If our array is large mine is optimal for a small max size there is a more optimal solution
+# Were there any bugs? Negative
+#  5 5 3 5 = 4.5
