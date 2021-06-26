e709d8c2a4b422dd5710998e928f15d5db761c3b
Kevin Knott <KKnott93@gmail.com>
2021-06-26 17:30:43 -0500

    Easily solved heap problem

--- Week 04/Day 05/a.py
+++ Week 04/Day 05/a.py
@@ -7,9 +7,35 @@
 # If x != y, the stone of weight x is totally destroyed, and the stone of weight y has new weight y-x.
 # At the end, there is at most 1 stone left.  Return the weight of this stone (or 0 if there are no stones left.)
 
+import heapq
+
+
 class Solution:
     def lastStoneWeight(self, stones) -> int:
-        return
+        # It is o(N) to heapify or we can go through and heappush in nlogn
+        # Although I just realized we need to use a max heap so we need to invert the sign
+        for i in range(len(stones)):
+            stones[i] *= -1
+
+        heapq.heapify(stones)
+
+        # we need to loop until we have size >= 2
+        while len(stones) >= 2:
+            y, x = heapq.heappop(stones) * -1, heapq.heappop(stones) * -1
+
+            if x == y:
+                continue
+            else:
+                heapq.heappush(stones, -1 * (y-x))
+
+        return 0 if len(stones) == 0 else (heapq.heappop(stones) * -1)
+
+# This works and is pretty optimal although I think we could improve the space complexity if we wanted to increase the
+# time complexity o(nlogn) and o(n) time/space
+
+# After looking at the solution mine is pretty optimal but apparently we can use a bucketing system/ counting sort to solve
+# this in a  linear time however this solution requires the bucketing to be of the same length of the highest stone
+# so it can quickly be outperformed by the previous algo
 
 
 # Score Card
