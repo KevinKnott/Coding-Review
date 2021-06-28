6473a38bc8ce42634f0d1c21e7234065796c4cd1
Kevin Knott <KKnoTT93@gmail.com>
2021-06-27 21:33:43 -0500

    I am actually impressed with myself I managed to come out with a graph solution easily in the alotted time

--- Week 04/Day 06/c.py
+++ Week 04/Day 06/c.py
@@ -10,14 +10,48 @@
 # A graph is bipartite if the nodes can be partitioned into two independent sets A and B such that every edge in the graph connects a node in set A and a node in set B.
 
 # Return true if and only if it is bipartite.
-
 class Solution:
     def isBipartite(self, graph) -> bool:
-        return
+        # ourGraph = {}
+
+        # # First we need to take our list and create our graph
+        # for node in graph:
+        #     for value in graph[node]:
+        #         ourGraph[node].add(value)
+
+        def dfs(index, color):
+            if index in visited:
+                if visited[index] != color:
+                    return False
+                return True
+
+            # Mark Color and Visisted
+            visited[index] = color
+
+            for i in graph[index]:
+                if not dfs(i, color ^ 1):
+                    return False
+
+            return True
+
+        visited = {}
+        result = True
+        # Then we need to start at a node color Red and go through B/R
+        # for i in our list and dont go to a visited node
+        for i in range(len(graph)):
+            if i not in visited:
+                result = result and dfs(i, 0)
+
+        return result
+
+# This solution works and it will run in O(V+E) and store o(V)  could we improve this?
+# I think we could I simply took the easy approach of using recursion but you could
+# definitely improve the performance slightly by converting this into a iterative solution
+# Also having to track the recursion result could be improved in the iterative approach
 
 # Score Card
 # Did I need hints? Nope
-# Did you finish within 30 min? 4 minutes
-# Was the solution optimal? Yup this runs in o(n+m) time in worst case and uses o(1) space
-# Were there any bugs? None
+# Did you finish within 30 min? 30
+# Was the solution optimal? See my blurb from above
+# Were there any bugs? I don't really have
 # 5 5 5 5 = 5
