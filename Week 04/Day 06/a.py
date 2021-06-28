eb08a3571b756f5d59cb4ad4cc2822a16ac4a28b
Kevin Knott <KKnoTT93@gmail.com>
2021-06-27 20:01:22 -0500

    Adding the problems for the day

---
+++ Week 04/Day 06/a.py
@@ -0,0 +1,25 @@
+# Binary Tree Maximum Path Sum: https://leetcode.com/problems/binary-tree-maximum-path-sum/
+
+# A path in a binary tree is a sequence of nodes where each pair of adjacent nodes in the sequence has an edge connecting them. A node can only appear in the sequence at most once. Note that the path does not need to pass through the root.
+# The path sum of a path is the sum of the node's values in the path.
+# Given the root of a binary tree, return the maximum path sum of any path.
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def maxPathSum(self, root: TreeNode) -> int:
+        return
+
+
+# Score Card
+# Did I need hints? N
+# Did you finish within 30 min? N (45 or so)
+# Was the solution optimal? I believe so we could make some slight optimization but this will run in o(n^2) because of the multiplicity we would go through once and then again to multiply
+#  and o(n) space
+# Were there any bugs? I listed bugs in the above code
+#  5 2 4 2 = 3.25
