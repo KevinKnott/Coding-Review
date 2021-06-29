e649004bdf18a881a127509757d249cfe2c8b6dd
Kevin Knott <KKnott93@gmail.com>
2021-06-28 20:13:41 -0500

    Adding problems for the last of week 4

---
+++ Week 04/Day 07/a.py
@@ -0,0 +1,18 @@
+# Decode String: https://leetcode.com/problems/decode-string/
+
+# Given an encoded string, return its decoded string.
+# The encoding rule is: k[encoded_string], where the encoded_string inside the square brackets is being repeated exactly k times. Note that k is guaranteed to be a positive integer.
+# You may assume that the input string is always valid; No extra white spaces, square brackets are well-formed, etc.
+# Furthermore, you may assume that the original data does not contain any digits and that digits are only for those repeat numbers, k. For example, there won't be input like 3a or 2[4].
+
+class Solution:
+    def decodeString(self, s: str) -> str:
+        return
+
+# Score Card
+# Did I need hints? N
+# Did you finish within 30 min? N (45 or so)
+# Was the solution optimal? I believe so we could make some slight optimization but this will run in o(n^2) because of the multiplicity we would go through once and then again to multiply
+#  and o(n) space
+# Were there any bugs? I listed bugs in the above code
+#  5 2 4 2 = 3.25
