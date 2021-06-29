8266cd4307f08d5fd14e434330c2441a99bc2af3
Kevin Knott <KKnott93@gmail.com>
2021-06-28 20:13:41 -0500

    Adding problems for the last of week 4

---
+++ Week 04/Day 07/d.py
@@ -0,0 +1,21 @@
+# All Nodes Distance K in Binary Tree: https://leetcode.com/problems/all-nodes-distance-k-in-binary-tree/
+
+# We are given a binary tree (with root node root), a target node, and an integer value k.
+# Return a list of the values of all nodes that have a distance k from the target node.  The answer can be returned in any order.
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def distanceK(self, root: TreeNode, target: TreeNode, k: int):
+        return
+# Score Card
+# Did I need hints? N
+# Did you finish within 30 min? 10
+# Was the solution optimal? This is optimal
+# Were there any bugs? No
+#  5 5 5 5 = 5
