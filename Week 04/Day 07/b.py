17138e6fe8a9922c1493cc9f244e27dfb52c8f69
Kevin Knott <KKnott93@gmail.com>
2021-06-28 21:04:07 -0500

    Kadane's Algo!

--- Week 04/Day 07/b.py
+++ Week 04/Day 07/b.py
@@ -2,13 +2,34 @@
 
 # Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
 
+# My initial thought is that if we are going across the array we can either start the subarray from where we are
+# or we can add the value so if that value is higher than adding the value we simply need to reset our val
+# and record if we have a new max
+
 class Solution:
     def maxSubArray(self, nums) -> int:
-        return
+        result = -float('inf')
+        curSum = -float('inf')
+
+        for num in nums:
+            result = max(result, curSum)
+            curSum = max(num, curSum + num)
+
+        return max(result, curSum)
+
+
+# This solution is actually correct and runs in o(n) and o(1) which is pretty optimal
+# luckily I know this from my days studing in college but apparently there is a version of
+# this solution that exists using a divide and conquer algo which makes sense because the
+# because you should be able to create contiguous subarrays down to size one and then
+# build up. Especially since the above algo is a dp problem with tabulation that being said
+# I think I won't be coding it up as it seems quite complicated and honestly doesn't
+# have enough improvement to warrent coding. Although it is an interesting idea.
+
 
 # Score Card
-# Did I need hints? Y
-# Did you finish within 30 min? Y
-# Was the solution optimal? Y although I messed up the thought process of how to solve the problem
-# Were there any bugs?  I didn't really have any bugs
-#  2 3 3 5 = 3.25
+# Did I need hints? N
+# Did you finish within 30 min? 5 min
+# Was the solution optimal? Yes
+# Were there any bugs?  None
+# 5 5 5 5 = 5
