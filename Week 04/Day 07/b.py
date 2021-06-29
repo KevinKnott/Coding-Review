3ed50cd3600feb875e66293235c83432cbee59d6
Kevin Knott <KKnott93@gmail.com>
2021-06-28 20:13:41 -0500

    Adding problems for the last of week 4

---
+++ Week 04/Day 07/b.py
@@ -0,0 +1,14 @@
+# Maximum Subarray: https://leetcode.com/problems/maximum-subarray/
+
+# Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+
+class Solution:
+    def maxSubArray(self, nums) -> int:
+        return
+
+# Score Card
+# Did I need hints? Y
+# Did you finish within 30 min? Y
+# Was the solution optimal? Y although I messed up the thought process of how to solve the problem
+# Were there any bugs?  I didn't really have any bugs
+#  2 3 3 5 = 3.25
