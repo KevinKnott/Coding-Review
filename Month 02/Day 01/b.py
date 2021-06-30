46e45f65b05987004ed0e7c0461e9eaeb3b0f7a0
Kevin Knott <KKnoTT93@gmail.com>
2021-06-29 21:35:42 -0500

    Adding the score

--- Month 02/Day 01/b.py
+++ Month 02/Day 01/b.py
@@ -62,6 +62,6 @@ class Solution:
 # Score Card
 # Did I need hints? Y
 # Did you finish within 30 min? Y
-# Was the solution optimal? Y although I messed up the thought process of how to solve the problem
+# Was the solution optimal? Y although I messed up the thought process of how to solve the problem with multiple edges
 # Were there any bugs?  I didn't really have any bugs
-#  2 3 3 5 = 3.25
+# 2 4 3 5 = 3.25
