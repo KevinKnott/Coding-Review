a0306f3e0071a8d8e5609a0e7b0fcd47ebf1a36a
Kevin Knott <KKnoTT93@gmail.com>
2021-06-29 23:03:17 -0500

    Easy peasy lemon squeezy bst problem

--- Month 02/Day 01/d.py
+++ Month 02/Day 01/d.py
@@ -2,6 +2,10 @@
 
 # Given the root node of a binary search tree and two integers low and high, return the sum of values of all nodes with a value in the inclusive range [low, high].
 
+# This problem is simple as all we really need to do is a dfs where we add the some of every node if it is between the range this would run in o(n) and probably isn't the best
+# we know that this tree is in order so we can actually only go down nodes that are within the range don't go lower than left and higher than right it is still o(n) however
+# it will have slightly better performance
+
 # Definition for a binary tree node.
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
@@ -12,8 +16,21 @@ class TreeNode:
 
 class Solution:
     def rangeSumBST(self, root: TreeNode, low: int, high: int) -> int:
-        return
+        def dfs(node):
+            # figure out if we can go down to the left
+            if node:
+                self.result += node.val if low <= node.val and node.val <= high else 0
+                if node.val > low:
+                    dfs(node.left)
+                if node.val < high:
+                    dfs(node.right)
 
+        self.result = 0
+        dfs(root)
+        return self.result
+
+# The slight optimization that I mentioned moved my performance to the top 98% on this problem from 47 %
+# So knowing about how binary search tree works really does help although it is o(n) for space and time
 
 # Score Card
 # Did I need hints? N
