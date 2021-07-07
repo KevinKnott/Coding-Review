404874a68b1157d848b5e0ba0e549f3db9a6f993
Kevin Knott <KKnoTT93@gmail.com>
2021-07-07 18:47:44 -0500

    Simple deque creation except with a set window size

--- Month 02/Week 02/Day 02/c.py
+++ Month 02/Week 02/Day 02/c.py
@@ -1,8 +1,91 @@
+# Design Circular Queue: https://leetcode.com/problems/design-circular-queue/
 
+# Design your implementation of the circular queue. The circular queue is a linear data structure in which the operations are performed based on FIFO (First In First Out) principle and the last position is connected back to the first position to make a circle. It is also called "Ring Buffer".
+
+# One of the benefits of the circular queue is that we can make use of the spaces in front of the queue. In a normal queue, once the queue becomes full, we cannot insert the next element even if there is a space in front of the queue. But using the circular queue, we can use the space to store new values.
+
+# Implementation the MyCircularQueue class:
+
+#     MyCircularQueue(k) Initializes the object with the size of the queue to be k.
+#     int Front() Gets the front item from the queue. If the queue is empty, return -1.
+#     int Rear() Gets the last item from the queue. If the queue is empty, return -1.
+#     boolean enQueue(int value) Inserts an element into the circular queue. Return true if the operation is successful.
+#     boolean deQueue() Deletes an element from the circular queue. Return true if the operation is successful.
+#     boolean isEmpty() Checks whether the circular queue is empty or not.
+#     boolean isFull() Checks whether the circular queue is full or not.
+
+# You must solve the problem without using the built-in queue data structure in your programming language.
+
+# So we actually need to use a Node Class here as this is essentially a deque
+
+class Node:
+    def __init__(self, value, nextNode=None):
+        self.value = value
+        self.next = nextNode
+
+
+class MyCircularQueue:
+
+    def __init__(self, k: int):
+        self.maxSize = k
+        self.head = None
+        self.tail = None
+        self.size = 0
+
+    def enQueue(self, value: int) -> bool:
+        if self.isFull():
+            return False
+
+        temp = Node(value)
+        if self.count == 0:
+            self.head = temp
+            self.tail = self.head
+        else:
+            self.tail.next = temp
+            self.tail = self.tail.next
+        self.size += 1
+        return True
+
+    def deQueue(self) -> bool:
+        if self.isEmpty():
+            return False
+
+        self.head = self.head.next
+        self.size -= 1
+        return True
+
+    def Front(self) -> int:
+        if self.isEmpty():
+            return -1
+        return self.head.value if self.head else None
+
+    def Rear(self) -> int:
+        if self.isEmpty():
+            return -1
+        return self.tail.value if self.tail else None
+
+    def isEmpty(self) -> bool:
+        return self.size == 0
+
+    def isFull(self) -> bool:
+        return self.size == self.maxSize
+
+
+# Your MyCircularQueue object will be instantiated and called as such:
+# obj = MyCircularQueue(k)
+# param_1 = obj.enQueue(value)
+# param_2 = obj.deQueue()
+# param_3 = obj.Front()
+# param_4 = obj.Rear()
+# param_5 = obj.isEmpty()
+# param_6 = obj.isFull()
+
+# Super simple design problem that I have crushed
+# I would list out the complexities bit it is all o(1) and the space is o(k) because we have to save the whole window
 
 # Score Card
 # Did I need hints? Nope
-# Did you finish within 30 min? 4 minutes
-# Was the solution optimal? Yup this runs in o(n+m) time in worst case and uses o(1) space
+# Did you finish within 30 min? 20
+# Was the solution optimal? See above
 # Were there any bugs? None
 # 5 5 5 5 = 5
