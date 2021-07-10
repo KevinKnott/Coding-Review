c0aea5a075824721a98d7a8d4d4e7883eb051a2f
Kevin Knott <KKnott93@gmail.com>
2021-07-09 23:00:14 -0500

    Adding problems 2-4 and the 2nd statement

---
+++ Month 02/Week 02/Day 04/b.py
@@ -0,0 +1,22 @@
+# Binary Tree Right Side View: https://leetcode.com/problems/binary-tree-right-side-view/
+
+# Given the root of a binary tree, imagine yourself standing on the right side of it, return the values of the nodes you can see ordered from top to bottom.
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def rightSideView(self, root: TreeNode):
+        return
+
+# Score Card
+# Did I need hints? Y
+# Did you finish within 30 min? Y
+# Was the solution optimal? Y although I messed up the thought process of how to solve the problem
+# Were there any bugs?  I didn't really have any bugs
+#  2 3 3 5 = 3.25
---
+++ Month 02/Week 02/Day 04/c.py
@@ -0,0 +1,8 @@
+
+
+# Score Card
+# Did I need hints? Nope
+# Did you finish within 30 min? 4 minutes
+# Was the solution optimal? Yup this runs in o(n+m) time in worst case and uses o(1) space
+# Were there any bugs? None
+# 5 5 5 5 = 5
