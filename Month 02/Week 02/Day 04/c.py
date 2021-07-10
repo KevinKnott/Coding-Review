c9f099a3a21c293a19463fae10ccb7c378a52915
Kevin Knott <KKnott93@gmail.com>
2021-07-09 23:12:17 -0500

    Adding problem 3!

--- Month 02/Week 02/Day 04/c.py
+++ Month 02/Week 02/Day 04/c.py
@@ -1,4 +1,27 @@
+# Find Median from Data Stream: https://leetcode.com/problems/find-median-from-data-stream/
 
+# The median is the middle value in an ordered integer list. If the size of the list is even, there is no middle value and the median is the mean of the two middle values.
+
+# For example, for arr = [2,3,4], the median is 3.
+# For example, for arr = [2,3], the median is (2 + 3) / 2 = 2.5.
+# Implement the MedianFinder class:
+
+# MedianFinder() initializes the MedianFinder object.
+# void addNum(int num) adds the integer num from the data stream to the data structure.
+# double findMedian() returns the median of all elements so far. Answers within 10-5 of the actual answer will be accepted.
+
+class MedianFinder:
+
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+
+    def addNum(self, num: int) -> None:
+        return
+
+    def findMedian(self) -> float:
+        return
 
 # Score Card
 # Did I need hints? Nope
