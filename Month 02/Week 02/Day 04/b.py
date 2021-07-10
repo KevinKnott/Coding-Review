5d352d25bc0ac4f8764daa5ae7075ce5fe666469
Kevin Knott <KKnott93@gmail.com>
2021-07-09 23:11:00 -0500

    Crushed the second problem where we simply do a BFS and keep track of the last node on every step

--- Month 02/Week 02/Day 04/b.py
+++ Month 02/Week 02/Day 04/b.py
@@ -3,20 +3,51 @@
 # Given the root of a binary tree, imagine yourself standing on the right side of it, return the values of the nodes you can see ordered from top to bottom.
 
 # Definition for a binary tree node.
+from collections import deque
+
+
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
 
+#  This solution is pretty simple if we do a bst and put a maker at the end of every level (or create a new q )
+#  we can simply append the last node we saw and return that list
+
 
 class Solution:
     def rightSideView(self, root: TreeNode):
-        return
+        if root is None:
+            return
+
+        nextLevel = deque()
+        nextLevel.appendleft(root)
+        result = []
+
+        while nextLevel:
+            curLevel = nextLevel
+            nextLevel = deque()
+
+            while curLevel:
+                node = curLevel.pop()
+
+                if node.left:
+                    nextLevel.appendleft(node.left)
+                if node.right:
+                    nextLevel.appendleft(node.right)
+
+            result.append(node.val)
+
+        return result
+
+# Man we are on a speed run today! This is an optimal O(N) time algo where we do a simple bfs
+# it takes o(N) space as well which I believe is optimal as you have to visit every node in the case
+# That the right most node is oon the bottom left
 
 # Score Card
-# Did I need hints? Y
-# Did you finish within 30 min? Y
-# Was the solution optimal? Y although I messed up the thought process of how to solve the problem
-# Were there any bugs?  I didn't really have any bugs
-#  2 3 3 5 = 3.25
+# Did I need hints? N
+# Did you finish within 30 min? 10 min
+# Was the solution optimal? See above
+# Were there any bugs? Nope
+# 5 5 5 5 = 5
