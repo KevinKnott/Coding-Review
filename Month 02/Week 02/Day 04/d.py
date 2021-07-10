e0f6edcdbd0cdd3bf4fd8fb1c058c1b256ea2f8e
Kevin Knott <KKnott93@gmail.com>
2021-07-09 23:33:36 -0500

    The last problem was a simple stack parsing problem

--- Month 02/Week 02/Day 04/d.py
+++ Month 02/Week 02/Day 04/d.py
@@ -7,14 +7,37 @@
 # Open brackets must be closed by the same type of brackets.
 # Open brackets must be closed in the correct order.
 
+# This is actually another super easy problem all we have to do is keep adding all values onto the stack
+# until we see a closing bracket then we check if the top of stack matches if it does continue else
+# return false if the stack is not empty at the end it is also invalid
+
 class Solution:
     def isValid(self, s: str) -> bool:
-        return
+        stack = []
 
+        match = {
+            ')': '(',
+            ']': '[',
+            '}': '{'
+        }
+
+        for char in s:
+            if char in match:
+                if len(stack) == 0 or stack[-1] != match[char]:
+                    return False
+
+                stack.pop()
+
+            elif char in '([{':
+                stack.append(char)
+
+        return len(stack) == 0
+
+# This runs in o(n) time and space and is optimal
 
 # Score Card
 # Did I need hints? N
-# Did you finish within 30 min? 10
+# Did you finish within 30 min? 7
 # Was the solution optimal? This is optimal
 # Were there any bugs? No
 #  5 5 5 5 = 5
