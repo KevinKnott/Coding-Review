8e95d2fcb4232b73ed22f7cb16a0064fd56ef3c4
Kevin Knott <KKnott93@gmail.com>
2021-07-09 22:54:35 -0500

    Adding problem 1!

---
+++ Month 02/Week 02/Day 04/a.py
@@ -0,0 +1,22 @@
+# Reverse Linked List II: https://leetcode.com/problems/reverse-linked-list-ii/
+
+# Given the head of a singly linked list and two integers left and right where left <= right, reverse the nodes of the list from position left to position right, and return the reversed list.
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def reverseBetween(self, head: ListNode, left: int, right: int) -> ListNode:
+        return
+
+# Score Card
+# Did I need hints? N
+# Did you finish within 30 min? N (45 or so)
+# Was the solution optimal? I believe so we could make some slight optimization but this will run in o(n^2) because of the multiplicity we would go through once and then again to multiply
+#  and o(n) space
+# Were there any bugs? I listed bugs in the above code
+#  5 2 4 2 = 3.25
