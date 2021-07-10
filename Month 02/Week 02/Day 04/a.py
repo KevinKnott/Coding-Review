c4430742cf609649cb4e92cf0364e41e4aa1dfeb
Kevin Knott <KKnott93@gmail.com>
2021-07-09 22:59:01 -0500

    Adding problem 1 which was a simple list reversal with some tricky moving around it

--- Month 02/Week 02/Day 04/a.py
+++ Month 02/Week 02/Day 04/a.py
@@ -11,12 +11,57 @@ class ListNode:
 
 class Solution:
     def reverseBetween(self, head: ListNode, left: int, right: int) -> ListNode:
-        return
+
+        # Definition for singly-linked list.
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+# So my intial thought for this problem is that we have a standard reversing of a linked list
+# There are two distinctions until we get to the node we simply keep iterating across
+# Then once we found the starting place we need to connect what we had before by storing a
+# pointer to the prev of where we started then we can go ahead and reverse until we reach right
+# then if we had a previous point it to the next otherwise update our head
+# after that we simply return the new head/ old head
+
+
+class Solution:
+    def reverseBetween(self, head: ListNode, left: int, right: int) -> ListNode:
+        if not head:
+            return None
+
+        cur = head
+        prev = None
+
+        while left > 1:
+            prev = cur
+            cur = cur.next
+            left, right = left - 1, right - 1
+
+        tail, con = cur, prev
+
+        while right > 0:
+            temp = cur.next
+            cur.next = prev
+            prev = cur
+            cur = temp
+            right -= 1
+
+        if con:
+            con.next = prev
+        else:
+            head = prev
+
+        tail.next = cur
+
+        return head
 
 # Score Card
-# Did I need hints? N
-# Did you finish within 30 min? N (45 or so)
-# Was the solution optimal? I believe so we could make some slight optimization but this will run in o(n^2) because of the multiplicity we would go through once and then again to multiply
-#  and o(n) space
-# Were there any bugs? I listed bugs in the above code
-#  5 2 4 2 = 3.25
+# Did I need hints? Nope
+# Did you finish within 30 min? 16 min
+# Was the solution optimal? This solution runs in o(n) time and o(1) space I believe it is optimal as we have no way of reversing without visiting all the nodes
+# Were there any bugs? I forgot that we need to keep track of whether the head swaps as part of the traversal
+# 4 4 5 5 = 4.5
