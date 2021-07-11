ca3eb62d21651b2c6d17885b9e41605b68e72a0e
Kevin Knott <KKnott93@gmail.com>
2021-07-11 14:58:53 -0500

    Adding the template for the day

---
+++ Month 02/Week 02/Day 06/a.py
@@ -0,0 +1,9 @@
+
+
+# Score Card
+# Did I need hints? N
+# Did you finish within 30 min? N (45 or so)
+# Was the solution optimal? I believe so we could make some slight optimization but this will run in o(n^2) because of the multiplicity we would go through once and then again to multiply
+#  and o(n) space
+# Were there any bugs? I listed bugs in the above code
+#  5 2 4 2 = 3.25
