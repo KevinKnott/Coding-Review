6bfd9f550075f640200029d4ba1cc3cd78b9631c
Kevin Knott <KKnoTT93@gmail.com>
2021-07-13 20:16:44 -0500

    Is a linked list node removal straight from Data Structures class

--- Month 02/Week 03/Day 10/b.py
+++ Month 02/Week 03/Day 10/b.py
@@ -8,14 +8,40 @@ class ListNode:
         self.val = val
         self.next = next
 
+# Simple removal separate out removing from the front because we have to move head
+# and removal from between the list
+
 
 class Solution:
     def removeElements(self, head: ListNode, val: int) -> ListNode:
-        return
+        # Remove from the head of a list
+        cur = head
+        while cur is not None and cur.val == val:
+            cur = cur.next
+
+        # Update head
+        head = cur
+
+        # Loop to the end
+        while cur is not None:
+            # If we see val
+            if cur.val == val:
+                # push over it
+                prev.next = cur.next
+            else:
+                # Otherwise we need to update our prev
+                prev = cur
+            # Continue looping
+            cur = cur.next
+
+        return head
+
+# The above works! It runs in o(N) time and o(1) space there isn't anything especially tricky about this problem
+# you just need to make sure you understand the different tops of removing a node from a linked list
 
 # Score Card
-# Did I need hints? Y
-# Did you finish within 30 min? Y
-# Was the solution optimal? Y although I messed up the thought process of how to solve the problem
-# Were there any bugs?  I didn't really have any bugs
-# 2 3 3 5 = 3.25
+# Did I need hints? N
+# Did you finish within 30 min? 9
+# Was the solution optimal? Yup
+# Were there any bugs? Nope
+# 5 5 5 5 = 5
