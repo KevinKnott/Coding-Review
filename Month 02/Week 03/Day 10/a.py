29830566d65c3630c16d63d12c31a860f234f2c5
Kevin Knott <KKnoTT93@gmail.com>
2021-07-13 20:04:36 -0500

    First problem was a simple dfs problem where you have to consider chosing the left path or right path or the path ending at current node plus left and right

--- Month 02/Week 03/Day 10/a.py
+++ Month 02/Week 03/Day 10/a.py
@@ -12,14 +12,34 @@ class TreeNode:
         self.right = right
 
 
+# Okay so this problem is looking for the longest possible paths through a tree. This seems like a simple dfs problem where we check
+# the left side the right side and either take left + Mid + right or mid + max(left, right)  but mid is ignored as we have a path of x - 1
+
 class Solution:
     def diameterOfBinaryTree(self, root: TreeNode) -> int:
-        return
+        self.result = 0
+
+        def dfs(node):
+            # When the node is empty we can return there is no path so our result is 0
+            if node is None:
+                return 0
+
+            # Traverse both sides
+            left = dfs(node.left)
+            right = dfs(node.right)
+
+            # Compare the whole path at each node
+            self.result = max(self.result, left + right)
+
+            # move up 1 node plus whatever path was highest to this point
+            return 1 + max(left, right)
+
+        dfs(root)
+        return self.result
 
 # Score Card
 # Did I need hints? N
-# Did you finish within 30 min? N (45 or so)
-# Was the solution optimal? I believe so we could make some slight optimization but this will run in o(n^2) because of the multiplicity we would go through once and then again to multiply
-#  and o(n) space
-# Were there any bugs? I listed bugs in the above code
-# 5 2 4 2 = 3.25
+# Did you finish within 30 min? 10
+# Was the solution optimal? Yup this is optimal o(N) and o(N) as we have to traverse every node with a stack
+# Were there any bugs? 0 bugs for me today
+# 5 5 5 5 = 5
