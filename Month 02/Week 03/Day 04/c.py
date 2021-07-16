76eab2a3cd5752223570af086ed25b84202a8302
Kevin Knott <KKnoTT93@gmail.com>
2021-07-16 18:41:38 -0500

    Simple parsing problem the key to this problem is learning how to sort on unique data types in this case by a key/val pair

--- Month 02/Week 03/Day 04/c.py
+++ Month 02/Week 03/Day 04/c.py
@@ -15,14 +15,39 @@
 
 # Return the final order of the logs.
 
+# So we are given an array of logs and we are to sort them by letter log, then digit log. Also the letter log is to be lexicographically sorted
+# Which basically means that this comes down to a simple parsing problem then sorting the letters and recombining the lists
 
 class Solution:
     def reorderLogFiles(self, logs):
-        return
+        letterLog = []
+        digitLog = []
+
+        # Since we have to go log by log we could inject the log in o(n) with an insertion sort
+        # but for simplicity I will just use the built in sort which is nlogn
+
+        for log in logs:
+            identifier, values = log.split(' ', 1)
+
+            # Now we need to check for if we have digit or letter
+            if values[0].isalpha():
+                letterLog.append([identifier, values])
+                letterLog.sort(key=lambda x: x[0])
+                letterLog.sort(key=lambda x: x[1])
+            else:
+                digitLog.append(log)
+
+        result = []
+
+        for i in range(len(letterLog)):
+            result.append(' '.join(letterLog[i]))
+
+        return result + digitLog
+
 
 # Score Card
 # Did I need hints? Nope
-# Did you finish within 30 min? 4 minutes
-# Was the solution optimal? Yup this runs in o(n+m) time in worst case and uses o(1) space
+# Did you finish within 30 min? 10
+# Was the solution optimal? Yup this is optimal and runs in O(M * N log N) as we go through all digit/letter logs and sort the letterlogs
 # Were there any bugs? None
 # 5 5 5 5 = 5
