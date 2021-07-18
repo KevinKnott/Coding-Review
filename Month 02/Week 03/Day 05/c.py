8aa085103dc8c74492bcc58a9c75b1bb056658e0
Kevin Knott <KKnott93@gmail.com>
2021-07-17 21:46:13 -0500

    Finishing up the simple product array

--- Month 02/Week 03/Day 05/c.py
+++ Month 02/Week 03/Day 05/c.py
@@ -4,13 +4,36 @@
 # The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
 # You must write an algorithm that runs in O(n) time and without using the division operation.
 
+# In this problem we can take advantage of the fact that we need to calc every other number except self
+# and from l -> r which will give you the product from l -> i and then we go through a second time and
+#  go from r -> l which will give you the product from r -> i which is the solution
+
+
 class Solution:
     def productExceptSelf(self, nums):
-        return
+        prefix, postfix = [0] * len(nums), [0] * len(nums)
+
+        curProd = 1
+        for i in range(len(nums)):
+            prefix[i] = curProd
+            curProd *= nums[i]
+
+        curProd = 1
+        for i in reversed(range(len(nums))):
+            postfix[i] = curProd
+            curProd *= nums[i]
+
+        return [prefix[i] * postfix[i] for i in range(len(nums))]
+
+
+# The above works pretty well! It runs in o(N) (technically 3n as we loop through the n nums 3 times)
+# Basically we use a dp table to keep a current product ove every number to the left in prefix
+# and then the same for  the product to the right of i in postfix  then all we have to do is multiply
+# l -> i * r -> i
 
 # Score Card
 # Did I need hints? Nope
-# Did you finish within 30 min? 4 minutes
-# Was the solution optimal? Yup this runs in o(n+m) time in worst case and uses o(1) space
+# Did you finish within 30 min? 10
+# Was the solution optimal? See the above
 # Were there any bugs? None
 # 5 5 5 5 = 5
