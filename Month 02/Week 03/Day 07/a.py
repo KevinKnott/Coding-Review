19a02cd0c5440fb5a67ed455b426af63963fae0b
Kevin Knott <KKnott93@gmail.com>
2021-07-19 20:40:27 -0500

    Problem one was a either sorting or hashing problem

--- Month 02/Week 03/Day 07/a.py
+++ Month 02/Week 03/Day 07/a.py
@@ -1,9 +1,35 @@
+# Group Anagrams: https://leetcode.com/problems/group-anagrams/
 
+# Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+
+# An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+# So the first solution that comes to mind is create a dicitonary of letters and counts of each letter and hashing them together
+# the problem with this is that dicts arent a hashable type so we must convert them to a tuple which is a o(n) step. Also tuples
+# can be used as the are immutable. Also instead of using dicts we should use a list of len 26 as it will take O(1) space and
+# allow us to not need to sort the letters in a dictionary
+
+from collections import defaultdict
+
+
+class Solution:
+    def groupAnagrams(self, strs):
+        result = defaultdict(list)
+
+        for word in strs:
+            hash = [0] * 26
+
+            for ltr in word:
+                hash[ord(ltr) - ord('a')] += 1
+
+            result[tuple(hash)].append(word)
+
+        return [v for v in result.values()]
 
 # Score Card
 # Did I need hints? N
-# Did you finish within 30 min? N (45 or so)
-# Was the solution optimal? I believe so we could make some slight optimization but this will run in o(n^2) because of the multiplicity we would go through once and then again to multiply
-#  and o(n) space
-# Were there any bugs? I listed bugs in the above code
-#  5 2 4 2 = 3.25
+# Did you finish within 30 min? 8 min
+# Was the solution optimal? This is optimal but runs in O(NK) for time and space where N is the num of words
+# and k is the len of longest word
+# Were there any bugs? Negative
+# 5 5 5 5 = 5
