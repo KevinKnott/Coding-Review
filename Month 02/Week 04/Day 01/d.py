d557b6550bd8f8e9f9f010c8c6d5e46627cd7597
Kevin Knott <KKnoTT93@gmail.com>
2021-07-20 23:27:43 -0500

    This is a really challenging dp problem that uses recursion and memoization plus pattern matching

--- Month 02/Week 04/Day 01/d.py
+++ Month 02/Week 04/Day 01/d.py
@@ -7,13 +7,50 @@
 
 # The matching should cover the entire input string (not partial).
 
+# This looks like a simple recursion problem which we may be able to add in memoization for piece we have already computed
+# For instance using a . is quite simple as you just move along the pattern but for * it is complicated because you either
+# move x chars down or potentially 0. Which honestly sounds like backtracking
+
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
-        return
+        memo = {}
+        lengthS, lengthP = len(s), len(p)
+
+        def backtrack(sIndex=0, pIndex=0):
+            # Create a key and check if we can return result we have seen
+            key = (sIndex, pIndex)
+            if key not in memo:
+
+                # Check if we have no more pattern to match because if s has values but p is empty we return false
+                if pIndex == lengthP:
+                    return sIndex == lengthS
+
+                # Check first if we have two letters so we can move down or check if we have a . as that is any one char
+                firstMatch = sIndex < lengthS and (
+                    p[pIndex] == s[sIndex] or p[pIndex] == '.')
+
+                # Check if the pattern as a posibility of *
+                if pIndex < lengthP - 1 and p[pIndex+1] == '*':
+                    # Here we check our two options where we have no * match and change pattern
+                    # or we match first char and have 0 -> x times so we move down s
+                    memo[key] = (
+                        backtrack(sIndex, pIndex + 2)) or (firstMatch and backtrack(sIndex+1, pIndex))
+                else:
+                    # If we dont have a valid star we check if the first match and continue down
+                    # this is the nomral possibility
+                    memo[key] = firstMatch and backtrack(
+                        sIndex+1, pIndex+1)
+
+                # Afterwards we need to propogate back up the backtracking
+                return memo[key]
+            else:
+                return memo[key]
+
+        return backtrack()
 
 # Score Card
-# Did I need hints? N
-# Did you finish within 30 min? 10
-# Was the solution optimal? This is optimal
+# Did I need hints? Y
+# Did you finish within 30 min? 30
+# Was the solution optimal? This is optimal but I honestly don't know the complexity probably O(TP) for time and space where T and P are the len of strings
 # Were there any bugs? No
-#  5 5 5 5 = 5
+# 2 2 3 4 = 2.75
