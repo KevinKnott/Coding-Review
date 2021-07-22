52cd6a08f39c3fa7486235de91725fa74873d0d8
Kevin Knott <KKnoTT93@gmail.com>
2021-07-21 21:28:30 -0500

    Easy peasy calculator with stacks and making sure signs are carried through the solution

--- Month 02/Week 04/Day 02/b.py
+++ Month 02/Week 04/Day 02/b.py
@@ -1,7 +1,51 @@
+# Basic Calculator: https://leetcode.com/problems/basic-calculator/
+
+# Given a string s representing a valid expression, implement a basic calculator to evaluate it, and return the result of the evaluation.
+# Note: You are not allowed to use any built-in function which evaluates strings as mathematical expressions, such as eval().
+
+# This solution is simple all we have to do is create a stack to store values if we see a paren
+# then once this is done we simply convert the numbers to only use addition by changing the sign if we see a negative
+
+
+class Solution:
+    def calculate(self, s: str) -> int:
+        stack = []
+        result, num, sign = 0, 0, 1
+
+        for i in range(len(s)):
+
+            if s[i] != ' ':
+
+                if s[i].isdigit():
+                    num = (num * 10) + int(s[i])
+                elif s[i] == '-':
+                    result += sign * num
+                    sign = - 1
+                    num = 0
+                elif s[i] == '+':
+                    result += sign * num
+                    sign = 1
+                    num = 0
+                elif s[i] == '(':
+                    stack.append(result)
+                    stack.append(sign)
+
+                    sign = 1
+                    result = 0
+
+                else:
+                    result += sign * num
+                    result *= stack.pop()
+                    result += stack.pop()
+                    num = 0
+
+        return result + (sign * num)
+
+# the above runs in O(N) time and space and is pretty basic
 
 # Score Card
-# Did I need hints? Y
-# Did you finish within 30 min? Y
-# Was the solution optimal? Y although I messed up the thought process of how to solve the problem
-# Were there any bugs?  I didn't really have any bugs
-#  2 3 3 5 = 3.25
+# Did I need hints? N
+# Did you finish within 30 min? 15
+# Was the solution optimal? Ohhhh yeaaa
+# Were there any bugs?  Nope
+# 5 5 5 5 = 5
