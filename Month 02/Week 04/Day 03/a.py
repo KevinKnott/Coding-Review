af478ee3f8855439a779a5cfc48fb5928f7f509f
Kevin Knott <KKnott93@gmail.com>
2021-07-22 23:37:56 -0500

    Adding Score

--- Month 02/Week 04/Day 03/a.py
+++ Month 02/Week 04/Day 03/a.py
@@ -22,8 +22,7 @@ class Solution:
 
 # Score Card
 # Did I need hints? N
-# Did you finish within 30 min? N (45 or so)
-# Was the solution optimal? I believe so we could make some slight optimization but this will run in o(n^2) because of the multiplicity we would go through once and then again to multiply
-#  and o(n) space
-# Were there any bugs? I listed bugs in the above code
-#  5 2 4 2 = 3.25
+# Did you finish within 30 min? 5
+# Was the solution optimal? This is optimal it runs in O(N) and uses o(1) space as we only have 3 vars
+# Were there any bugs? Nope
+# 5 5 5 5 = 5
