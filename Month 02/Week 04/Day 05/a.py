3928d2ff5b0e70cdb1600191e9baf4f9a78f9e6d
Kevin Knott <KKnoTT93@gmail.com>
2021-07-28 21:58:47 -0500

    Reversing K nodes is just a combination of recursion and the basic reversing of a linked list

--- Month 02/Week 04/Day 05/a.py
+++ Month 02/Week 04/Day 05/a.py
@@ -9,16 +9,52 @@ class ListNode:
         self.val = val
         self.next = next
 
+# Okay this problem can actually  be broken into two separate pieces reveresing nodes (super easy)
+# and iterating through the linked list the tricky part is how do we actaully point the end to the next piece
+# because for instance in the first swap you actually get a new head right
+
 
 class Solution:
     def reverseKGroup(self, head: ListNode, k: int) -> ListNode:
-        return
+
+        # For this part of the algo we simply need to loop through and see if we can
+        # actually reverse k nodes
+        cur = head
+        count = 0
+        while cur and count != k:
+            cur = cur.next
+            count += 1
+
+        # Now that we know we have a k group we need to  reverse just that group
+        if count == k:
+            # Simply reverse the group we just saw
+            reversed = self.reverse(head, k)
+            # Try reversing the next k group and if you can
+            # attach it to the end of our cur which is actually head
+            # as we have reversed the array
+            head.next = self.reverseKGroup(cur, k)
+            return reversed
+
+        return head
+
+    # The reversal is easy it is the standard algo except you swap k elements
+    def reverse(self, head, k):
+        prev, cur = None, head
+        while k > 0:
+            temp = cur.next
+            cur.next = prev
+            prev = cur
+            cur = temp
+            k -= 1
+
+        return prev
 
 
 # Score Card
 # Did I need hints? N
-# Did you finish within 30 min? N (45 or so)
-# Was the solution optimal? I believe so we could make some slight optimization but this will run in o(n^2) because of the multiplicity we would go through once and then again to multiply
-#  and o(n) space
-# Were there any bugs? I listed bugs in the above code
-#  5 2 4 2 = 3.25
+# Did you finish within 30 min? 18
+# Was the solution optimal? This solution runs in o(N) time and space. This isn't the most optimal as you technically could
+# remove the use of a stack and do this completely iteratively and using extra nodes but it adds a lot of complexity
+# and I don't have enough time to code it
+# Were there any bugs? No bugs
+#  5 5 3 5 = 4.5
