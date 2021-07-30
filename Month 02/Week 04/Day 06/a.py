64fc8d42c2098156f191ff9a8ea8dcd12ec3a489
Kevin Knott <KKnoTT93@gmail.com>
2021-07-29 20:05:48 -0500

    All problem definitions have been added

--- Month 02/Week 04/Day 06/a.py
+++ Month 02/Week 04/Day 06/a.py
@@ -1,4 +1,15 @@
+# Regular Expression Matching: https://leetcode.com/problems/regular-expression-matching/
 
+# Given an input string s and a pattern p, implement regular expression matching with support for '.' and '*' where:
+
+#     '.' Matches any single character.​​​​
+#     '*' Matches zero or more of the preceding element.
+
+# The matching should cover the entire input string (not partial).
+
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+        return
 
 # Score Card
 # Did I need hints? N
--- Month 02/Week 04/Day 06/d.py
+++ Month 02/Week 04/Day 06/d.py
@@ -1,3 +1,11 @@
+# Top K Frequent Elements: https://leetcode.com/problems/top-k-frequent-elements/
+
+# Given an integer array nums and an integer k, return the k most frequent elements. You may return the answer in any order.
+
+class Solution:
+    def topKFrequent(self, nums, k: int):
+        return
+
 
 # Score Card
 # Did I need hints? N
