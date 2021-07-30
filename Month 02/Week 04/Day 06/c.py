9ebf7cfccc1627222c2b8dd5fddcf4bc3a37adc7
Kevin Knott <KKnoTT93@gmail.com>
2021-07-29 22:19:24 -0500

    Change problem to the correct version as I accidently did b twice

--- Month 02/Week 04/Day 06/c.py
+++ Month 02/Week 04/Day 06/c.py
@@ -1,8 +1,8 @@
-# Task Scheduler: https://leetcode.com/problems/task-scheduler/
+# Top K Frequent Elements: https://leetcode.com/problems/top-k-frequent-elements/
 
-# Given a characters array tasks, representing the tasks a CPU needs to do, where each letter represents a different task. Tasks could be done in any order. Each task is done in one unit of time. For each unit of time, the CPU could complete either one task or just be idle.
-# However, there is a non-negative integer n that represents the cooldown period between two same tasks (the same letter in the array), that is that there must be at least n units of time between any two same tasks.
-# Return the least number of units of times that the CPU will take to finish all the given tasks.
+# Given an integer array nums and an integer k, return the k most frequent elements. You may return the answer in any order.
+
+#
 
 class Solution:
     def leastInterval(self, tasks, n: int) -> int:
