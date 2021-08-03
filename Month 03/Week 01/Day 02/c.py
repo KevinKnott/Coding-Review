521751d7dcfaf596ce649d446416b7e776c01ad7
Kevin Knott <KKnoTT93@gmail.com>
2021-08-02 22:16:31 -0500

    Problem number 3 my lord

--- Month 03/Week 01/Day 02/c.py
+++ Month 03/Week 01/Day 02/c.py
@@ -1,4 +1,11 @@
+# Number of Connected Components in an Undirected Graph: https://leetcode.com/problems/number-of-connected-components-in-an-undirected-graph/
 
+# You have a graph of n nodes. You are given an integer n and an array edges where edges[i] = [ai, bi] indicates that there is an edge between ai and bi in the graph.
+# Return the number of connected components in the graph.
+
+class Solution:
+    def countComponents(self, n: int, edges) -> int:
+        return
 
 # Score Card
 # Did I need hints? Nope
