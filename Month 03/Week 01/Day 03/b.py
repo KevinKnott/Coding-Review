b540c8227a66cdbcc233a93b909e68d18f194284
Kevin Knott <KKnoTT93@gmail.com>
2021-08-03 22:21:41 -0500

    Crushing simple for loop with a min/max check

--- Month 03/Week 01/Day 03/b.py
+++ Month 03/Week 01/Day 03/b.py
@@ -4,14 +4,51 @@
 # You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
 # Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
 
+# So in this problem we are checking to see if we can find two peaks one low and one high
+# so basically we check at every number and see if it is a new lo if not we check
+# if we could get a better max by selling the diff between the lo and the current num
+
+import math
+
+
 class Solution:
     def maxProfit(self, prices) -> int:
-        return
+        lo = math.inf
+        result = 0
+
+        for price in prices:
+            if price < lo:
+                lo = price
+            else:
+                result = max(result, price - lo)
+
+        return result
+
+# The above works and runs in o(N) time and o(1) space
+# while we can't improve its worst case complexity
+# we can make it slightly more efficient to do this we simply need to
+# make lo automatically the lowest number and then return result if we only
+# have 1 element
+    def maxProfit(self, prices) -> int:
+        if len(prices) <= 1:
+            return 0
+
+        lo, result = prices[0], 0
+
+        for i in range(1, len(prices)):
+            if prices[i] < lo:
+                lo = prices[i]
+            else:
+                result = max(result, prices[i] - lo)
+
+        return result
+
+# The above changes is a nominally faster result so it isn't worth it really
 
 
 # Score Card
-# Did I need hints? Y
-# Did you finish within 30 min? Y
-# Was the solution optimal? Y although I messed up the thought process of how to solve the problem
-# Were there any bugs?  I didn't really have any bugs
-#  2 3 3 5 = 3.25
+# Did I need hints? N
+# Did you finish within 30 min? 6
+# Was the solution optimal? Yee
+# Were there any bugs? Hard no
+# 5 5 5 5 = 5
