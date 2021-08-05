fcf6fa983923a7a5f22eed9adb366fd46a62414d
Kevin Knott <KKnoTT93@gmail.com>
2021-08-04 20:40:23 -0500

    New day new problem

---
+++ Month 03/Week 01/Day 04/a.py
@@ -0,0 +1,30 @@
+# Validate Binary Search Tree: https://leetcode.com/problems/validate-binary-search-tree/
+
+# Given the root of a binary tree, determine if it is a valid binary search tree (BST).
+
+# A valid BST is defined as follows:
+
+#     The left subtree of a node contains only nodes with keys less than the node's key.
+#     The right subtree of a node contains only nodes with keys greater than the node's key.
+#     Both the left and right subtrees must also be binary search trees.
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def isValidBST(self, root: TreeNode) -> bool:
+        return
+
+
+# Score Card
+# Did I need hints? N
+# Did you finish within 30 min? N (45 or so)
+# Was the solution optimal? I believe so we could make some slight optimization but this will run in o(n^2) because of the multiplicity we would go through once and then again to multiply
+#  and o(n) space
+# Were there any bugs? I listed bugs in the above code
+#  5 2 4 2 = 3.25
