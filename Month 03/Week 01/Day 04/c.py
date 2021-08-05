2f1db393d50d7d1bdcd0dbf7f4fbc9ec1944890a
Kevin Knott <KKnoTT93@gmail.com>
2021-08-04 21:29:53 -0500

    Crushing a q design problem

--- Month 03/Week 01/Day 04/c.py
+++ Month 03/Week 01/Day 04/c.py
@@ -16,33 +16,51 @@
 
 # Follow-up: Can you implement the queue such that each operation is amortized O(1) time complexity? In other words, performing n operations will take overall O(n) time even if one of those operations may take longer.
 
+# So the clever thing about using two stacks to define is that you can simply move the whole stack 1 to stack 2 to get the correct order
+# then we have x number of pops until we need to rebalance but moving stack 1 onto stack 2 this only occurs when stack 2 is empty
+
 class MyQueue:
 
     def __init__(self):
         """
         Initialize your data structure here.
         """
+        self.stack1 = []
+        self.stack2 = []
 
     def push(self, x: int) -> None:
         """
         Push element x to the back of queue.
         """
+        self.stack1.append(x)
 
     def pop(self) -> int:
         """
         Removes the element from in front of queue and returns that element.
         """
+        self.peek()
+
+        # The below would normally need an error check but since the problem said
+        # the calls are always valid this is okay
+        return self.stack2.pop()
 
     def peek(self) -> int:
         """
         Get the front element.
         """
+        if len(self.stack2) != 0:
+            return self.stack2[-1]
+
+        while self.stack1:
+            self.stack2.append(self.stack1.pop())
+
+        return self.stack2[-1]
 
     def empty(self) -> bool:
         """
         Returns whether the queue is empty.
         """
-
+        return len(self.stack1) + len(self.stack2) == 0
 
 # Your MyQueue object will be instantiated and called as such:
 # obj = MyQueue()
@@ -51,9 +69,12 @@ class MyQueue:
 # param_3 = obj.peek()
 # param_4 = obj.empty()
 
+# Boo to the yeah so this will run in O(1) for all but peek and pop which would be an amortized o(1)
+# as for space we obviously need o(N) space to store all the values
+
 # Score Card
-# Did I need hints? Nope
-# Did you finish within 30 min? 4 minutes
-# Was the solution optimal? Yup this runs in o(n+m) time in worst case and uses o(1) space
+# Did I need hints? No
+# Did you finish within 30 min? 12
+# Was the solution optimal? Yup
 # Were there any bugs? None
 # 5 5 5 5 = 5
