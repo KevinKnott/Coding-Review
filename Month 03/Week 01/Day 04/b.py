3e372206a220cbf32324525a29d3e0829ab79bc1
Kevin Knott <KKnoTT93@gmail.com>
2021-08-04 20:56:59 -0500

    Number two my lord

--- Month 03/Week 01/Day 04/b.py
+++ Month 03/Week 01/Day 04/b.py
@@ -1,3 +1,13 @@
+# Add Strings: https://leetcode.com/problems/add-strings/
+
+# Given two non-negative integers, num1 and num2 represented as string, return the sum of num1 and num2 as a string.
+# You must solve the problem without using any built-in library for handling large integers (such as BigInteger). You must also not convert the inputs to integers directly.
+
+
+class Solution:
+    def addStrings(self, num1: str, num2: str) -> str:
+
+        return
 
 # Score Card
 # Did I need hints? Y
