62ec95826bf41690e675efb2f4ac61e96c7a63b6
Kevin Knott <KKnoTT93@gmail.com>
2021-08-06 15:32:31 -0500

    Easy problem number 2 using the array as a set with negative numbers

--- Month 03/Week 01/Day 06/b.py
+++ Month 03/Week 01/Day 06/b.py
@@ -4,13 +4,30 @@
 # There is only one repeated number in nums, return this repeated number.
 # You must solve the problem without modifying the array nums and uses only constant extra space.
 
+# So the naive solution to this problem since we have to use constant space is to simply use two pointers
+# and do an n^2 search through the list. We could also probably do some smart sorting as well to check
+# in nlogn time but the optimal solution is just taking the numbers and converting them to be negative if seen
+# so if we go to check a number and it is already negative that is obviously the duplicate and we can return
+# this is an o(n) approach
+
 class Solution:
     def findDuplicate(self, nums) -> int:
-        return
+        for i in range(len(nums)):
+            # The index should be a positive number always
+            potentiallySeen = abs(nums[i]) - 1
+
+            if nums[potentiallySeen] < 0:
+                return potentiallySeen + 1
+
+            nums[potentiallySeen] *= -1
+
+        return -1
+
+# I am crusing through this problem this runs in O(N) and uses O(1)
 
 # Score Card
-# Did I need hints? Y
-# Did you finish within 30 min? Y
-# Was the solution optimal? Y although I messed up the thought process of how to solve the problem
-# Were there any bugs?  I didn't really have any bugs
-#  2 3 3 5 = 3.25
+# Did I need hints? N
+# Did you finish within 30 min? 5
+# Was the solution optimal? Yuh
+# Were there any bugs?  None
+# 5 5 5 5 = 5
