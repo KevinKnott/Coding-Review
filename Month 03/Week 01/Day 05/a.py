8180bafb6d47ca4a0a8cde83f2bede0634059d8a
Kevin Knott <KKnoTT93@gmail.com>
2021-08-05 21:50:55 -0500

    First problem is to create a tree and than traverse it so it was pretty simple

--- Month 03/Week 01/Day 05/a.py
+++ Month 03/Week 01/Day 05/a.py
@@ -5,16 +5,46 @@
 # When a process is killed, all of its children processes will also be killed.
 # Given an integer kill representing the ID of a process you want to kill, return a list of the IDs of the processes that will be killed. You may return the answer in any order.
 
+# This problem can be simplified if we go ahead and do an iteration where setup a tree and make sure that this new class includes a parent id.
+# once we do that we simply do a bfs from the node we are killing and wallah we have an easy o(N) solution
+
+from collections import defaultdict, deque
+
 
 class Solution:
     def killProcess(self, pid, ppid, kill):
-        return
+        if kill == 0:
+            return pid
 
+        ourGraph = defaultdict(list)
+
+        # Basically since pid and ppid are linked we can
+        # zip them together and for every ppid append the pid as a child
+        for node, parent in zip(pid, ppid):
+            ourGraph[parent].append(node)
+
+        # Now all we have to do is create a queue that starts at kill and move our way down
+        q = deque()
+        q.appendleft(kill)
+
+        result = []
+        while q:
+            node = q.pop()
+            result.append(node)
+
+            for child in ourGraph[node]:
+                q.appendleft(child)
+
+        return result
+
+
+# This solution actually helps us a lot in the avg case as we can remove anything outside the scope of a tree rooted at kill
+# this means it is more efficient the deeper the depth. However in worst case we still have to iterate over the whole tree
+# this runs in O(N) time and space because of this (and creating the tree itself)
 
 # Score Card
 # Did I need hints? N
-# Did you finish within 30 min? N (45 or so)
-# Was the solution optimal? I believe so we could make some slight optimization but this will run in o(n^2) because of the multiplicity we would go through once and then again to multiply
-#  and o(n) space
-# Were there any bugs? I listed bugs in the above code
-#  5 2 4 2 = 3.25
+# Did you finish within 30 min? 10
+# Was the solution optimal? Awwwww yeet
+# Were there any bugs? Negative there were no bugs to squash
+# 5 5 5 5 = 5
