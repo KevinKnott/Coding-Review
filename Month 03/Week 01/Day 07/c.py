e34f3b9464f39f5cd2ee53d490c0003735ccfb10
Kevin Knott <KKnoTT93@gmail.com>
2021-08-09 22:19:04 -0500

    Dawn of the third day

--- Month 03/Week 01/Day 07/c.py
+++ Month 03/Week 01/Day 07/c.py
@@ -1,4 +1,19 @@
+# Reverse Nodes in k-Group: https://leetcode.com/problems/reverse-nodes-in-k-group/
 
+# Given a linked list, reverse the nodes of a linked list k at a time and return its modified list.
+# k is a positive integer and is less than or equal to the length of the linked list. If the number of nodes is not a multiple of k then left-out nodes, in the end, should remain as it is.
+# You may not alter the values in the list's nodes, only nodes themselves may be changed.
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def reverseKGroup(self, head, k: int):
+        return
 
 # Score Card
 # Did I need hints? Nope
