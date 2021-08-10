018d909c94e567d6a404310a1fd8131152c2e910
Kevin Knott <KKnoTT93@gmail.com>
2021-08-09 22:39:57 -0500

    Neat linked list reversal problem, I need to come back to it and solve with an O(1) space solution though

--- Month 03/Week 01/Day 07/c.py
+++ Month 03/Week 01/Day 07/c.py
@@ -11,13 +11,53 @@ class ListNode:
         self.next = next
 
 
+# So for this problem I think that all we have is our classic reversal problem except
+# we need to use recursion after every time we reverse so that we can swap every k nodes
+
+
 class Solution:
     def reverseKGroup(self, head, k: int):
-        return
+        # we need to find out if there are k nodes to swap
+        count = 0
+        cur = head
+        while cur and count < k:
+            cur = cur.next
+            count += 1
+
+        if count == k:
+            # then we swap from head to k
+            newHead = self.reverse(head, k)
+
+            # and keep the cur which be at the end of the reversal and only the unprocessed values and our tail will be head
+            # we will then call reverseKgroup for head.next (we use cur as the next spot)
+            head.next = self.reverseKGroup(cur, k)
+            return newHead
+
+        return head
+
+    # Basic reversal
+
+    def reverse(self, head, k):
+        prev, cur = None,  head
+
+        for _ in range(k):
+            temp = cur.next
+            cur.next = prev
+            prev = cur
+            cur = temp
+
+        return prev
+
+
+# This problem takes a bit of thinking but isn't too hard. I highly advise drawing out this problem as it really does help quite a bit
+# The reversal isn't too complicated but thinking of how the values change is hard otherwise
+
+# The above runs in O(N) and uses o(N/K) space for the stack. I think there is probably an optimization of this problem where
+# we dont have recursion but I think it would be rather complicated.
 
 # Score Card
 # Did I need hints? Nope
-# Did you finish within 30 min? 4 minutes
-# Was the solution optimal? Yup this runs in o(n+m) time in worst case and uses o(1) space
+# Did you finish within 30 min? 10 min
+# Was the solution optimal? Optimal for time not space. I don't have a lot of time today
 # Were there any bugs? None
-# 5 5 5 5 = 5
+# 5 5 3 5 = 4.5
