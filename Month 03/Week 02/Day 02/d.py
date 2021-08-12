0b7abbaa2d0cd88beec81ab4531af50e0ff1d73c
Kevin Knott <KKnoTT93@gmail.com>
2021-08-11 22:50:07 -0500

    Yet another tree traversal but level order this time, taking care to recorde the last element (could easily be first element tho)

--- Month 03/Week 02/Day 02/d.py
+++ Month 03/Week 02/Day 02/d.py
@@ -4,20 +4,53 @@
 
 # Definition for a binary tree node.
 
+from collections import deque
+
+
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
 
+# In this problem what we have is a level order traversal in which we record the last element
+# There are a couple ways to do this one with push a None val at the end of every level
+# and keeping prev. Or you could just create the level as a new q and when the q has one element
+# you push it onto the result
+
 
 class Solution:
     def rightSideView(self, root):
-        return
+        if not root:
+            return
+
+        result = []
+        nextLevel = deque()
+        nextLevel.appendleft(root)
+
+        while nextLevel:
+            curLevel = nextLevel
+            nextLevel = deque()
+
+            while curLevel:
+                cur = curLevel.pop()
+
+                if cur.left:
+                    nextLevel.appendleft(cur.left)
+                if cur.right:
+                    nextLevel.appendleft(cur.right)
+
+                if len(curLevel) == 0:
+                    result.append(cur.val)
+
+        return result
+
+# Easy peasy lemon squeezy. This is a pretty basic level order traversal with a bfs the only trick is making sure
+# you are resetting the nextLevel as lists  are pass by reference
 
 # Score Card
 # Did I need hints? N
 # Did you finish within 30 min? 10
 # Was the solution optimal? This is optimal
 # Were there any bugs? No
-#  5 5 5 5 = 5
+# 5 5 5 5 = 5
