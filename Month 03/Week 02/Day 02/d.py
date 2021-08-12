74984b32029d21301a7b8a2de5e94ed4d64cea92
Kevin Knott <KKnoTT93@gmail.com>
2021-08-11 21:38:32 -0500

    Adding all of the problems for the day

--- Month 03/Week 02/Day 02/d.py
+++ Month 03/Week 02/Day 02/d.py
@@ -1,3 +1,19 @@
+# Binary Tree Right Side View: https://leetcode.com/problems/binary-tree-right-side-view/
+
+# Given the root of a binary tree, imagine yourself standing on the right side of it, return the values of the nodes you can see ordered from top to bottom.
+
+# Definition for a binary tree node.
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def rightSideView(self, root):
+        return
 
 # Score Card
 # Did I need hints? N
