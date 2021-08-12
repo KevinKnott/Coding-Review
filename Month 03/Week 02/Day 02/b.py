035ed50a65f65fe3b02fa270230a2d7bec284904
Kevin Knott <KKnoTT93@gmail.com>
2021-08-11 22:29:05 -0500

    Another graph traversal problem except we have duplicate tickets

--- Month 03/Week 02/Day 02/b.py
+++ Month 03/Week 02/Day 02/b.py
@@ -5,14 +5,51 @@
 #     For example, the itinerary ["JFK", "LGA"] has a smaller lexical order than ["JFK", "LGB"].
 # You may assume all tickets form at least one valid itinerary. You must use all the tickets once and only once.
 
+# Okay just looking at this problem I can see that it is a backtracking dfs problem as we have to try and complete the path of N tickets
+# and also do it in the most lexographical way. To do this I will take two passes one to create a graph that stores all of the tickets
+# in lexigraphical order and then another in which I do a simple backtracking dfs until I have used all N of the tickets
+
+from collections import defaultdict
+
+
 class Solution:
     def findItinerary(self, tickets):
-        return
+        ourGraph = defaultdict(list)
 
+        for src, dest in tickets:
+            ourGraph[src].append(dest)
+
+        # Now that we have everything in our list we need to sort and
+        # setup the number of tickets we have in visited
+        visited = {}
+        for src, dest in ourGraph.items():
+            dest.sort()
+            visited[src] = [False] * len(dest)
+
+        self.result = []
+
+        # From here all we need is a backtracking dfs
+        def backtrack(node='JFK', path=[]):
+            if len(path) == len(tickets):
+                self.result = path[:] + [node]
+
+            for i, dest in enumerate(ourGraph[node]):
+                # Dont use used tickets
+                if len(self.result) == 0 and visited[node][i] is False:
+                    visited[node][i] = True
+                    backtrack(dest, path + [node])
+                    visited[node][i] = False
+
+        backtrack()
+        return self.result
+
+# Easy peasy problem basically this is a simple backtracking problem the hard part about this problem is that we
+# have multiple tickets to the same place which makes this a bit more difficult. We get around this with sorting
+# and also using a bitset for our visited so we know if we have used every ticket
 
 # Score Card
-# Did I need hints? Y
-# Did you finish within 30 min? Y
-# Was the solution optimal? Y although I messed up the thought process of how to solve the problem
-# Were there any bugs?  I didn't really have any bugs
-#  2 3 3 5 = 3.25
+# Did I need hints? N
+# Did you finish within 30 min? 10
+# Was the solution optimal? Yuh
+# Were there any bugs? Nah
+# 5 5 5 5 = 5
