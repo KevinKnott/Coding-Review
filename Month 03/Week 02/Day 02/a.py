cd8750f49dfa39199f2774109d207274be939257
Kevin Knott <KKnoTT93@gmail.com>
2021-08-11 22:11:59 -0500

    Easy bfs problem with a bit of caching!

--- Month 03/Week 02/Day 02/a.py
+++ Month 03/Week 02/Day 02/a.py
@@ -6,20 +6,62 @@
 
 
 # Definition for a Node.
+from collections import deque
+
+
 class Node:
     def __init__(self, val=0, neighbors=None):
         self.val = val
         self.neighbors = neighbors if neighbors is not None else []
 
+# My initial solution to this problem is to traverse it twice once to simply create all nodes
+# and store them in a hashmap and then a second traversal to link the connected neighbors
+
 
 class Solution:
     def cloneGraph(self, node: 'Node') -> 'Node':
-        return
+        if not node:
+            return
+
+        ourGraph = {}
+        q = deque()
+        q.appendleft(node)
+
+        while q:
+            cur = q.pop()
+
+            if cur.val in ourGraph:
+                continue
+
+            ourGraph[cur.val] = Node(cur.val)
+
+            for nei in cur.neighbors:
+                q.appendleft(nei)
+
+        q.appendleft(node)
+        visited = set()
+        while q:
+            cur = q.pop()
+
+            if cur.val in visited:
+                continue
+
+            visited.add(cur.val)
+            for nei in cur.neighbors:
+                ourGraph[cur.val].neighbors.append(ourGraph[nei.val])
+                q.appendleft(nei)
+
+        return ourGraph[node.val]
+
+
+# Boo yeah this works in O(N+M) time and space and is optimal! It comes down to a special
+# dfs or bfs that has you create new nodes and store them as you go. I think you could probably
+# optimize the second bfs just a bit by only using the cur.val instead of the whole node in the
+# q but it is still the same overal complexity
 
 # Score Card
 # Did I need hints? N
-# Did you finish within 30 min? N (45 or so)
-# Was the solution optimal? I believe so we could make some slight optimization but this will run in o(n^2) because of the multiplicity we would go through once and then again to multiply
-#  and o(n) space
-# Were there any bugs? I listed bugs in the above code
-#  5 2 4 2 = 3.25
+# Did you finish within 30 min? 10
+# Was the solution optimal? Yee yee
+# Were there any bugs? Nope
+# 5 5 5 5 = 5
