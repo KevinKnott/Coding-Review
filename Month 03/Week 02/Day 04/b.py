c369d73443edbdcf76ca2b0431df65c3aac32e8a
Kevin Knott <KKnott93@gmail.com>
2021-08-13 22:24:44 -0500

    Problem two is another stack parsing problem that you can trick into an optimal solution by using a tricky stack of (key, count) on your stack

--- Month 03/Week 02/Day 04/b.py
+++ Month 03/Week 02/Day 04/b.py
@@ -4,13 +4,39 @@
 # We repeatedly make k duplicate removals on s until we no longer can.
 # Return the final string after all such duplicate removals have been made. It is guaranteed that the answer is unique.
 
+# This problem basically screams to use a stack. Basically we can add letters as we parse along with the count of how many there are
+# once we reach k we can pop it off otherwise we leave it alone. Then at the end we parse through the string of pairs and construct
+# the valid string
+
 class Solution:
     def removeDuplicates(self, s: str, k: int) -> str:
-        return
+        stack = []
+
+        for char in s:
+            if len(stack) > 0 and stack[-1][0] == char:
+                stack[-1][1] += 1
+
+                if stack[-1][1] == k:
+                    # Remove a char that has k element
+                    stack.pop()
+            else:
+                stack.append([char, 1])
+
+        result = ''
+        for i in range(len(stack)):
+            char, count = stack[i]
+            result += char * count
+
+        return result
+
+# The above works and is pretty efficient it uses o(N) time and space the question is can we do better?
+# I think that we could probably solve this slightly more efficiently with a two pointer solution
+# keeping track of what is kept and what we have parsed but at the same time it would still require
+# us to keep a stack of counts and is probably a bit more confusing
 
 # Score Card
-# Did I need hints? Y
-# Did you finish within 30 min? Y
-# Was the solution optimal? Y although I messed up the thought process of how to solve the problem
+# Did I need hints? N
+# Did you finish within 30 min? 10
+# Was the solution optimal? Mine is optimal
 # Were there any bugs?  I didn't really have any bugs
-#  2 3 3 5 = 3.25
+# 5 5 5 5 = 5
