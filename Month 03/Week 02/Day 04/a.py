4e053dc7e44d9573d467e2c4de9eba3642912a98
Kevin Knott <KKnott93@gmail.com>
2021-08-13 21:53:42 -0500

    Problem definition 1

--- Month 03/Week 02/Day 04/a.py
+++ Month 03/Week 02/Day 04/a.py
@@ -1,4 +1,18 @@
+# Add Two Numbers II: https://leetcode.com/problems/add-two-numbers-ii/
 
+# You are given two non-empty linked lists representing two non-negative integers. The most significant digit comes first and each of their nodes contains a single digit.
+# Add the two numbers and return the sum as a linked list. You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        return
 
 # Score Card
 # Did I need hints? N
