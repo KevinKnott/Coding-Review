0cfee8c165122a2921af62535fead9b731ad4299
Kevin Knott <KKnott93@gmail.com>
2021-08-13 21:56:19 -0500

    Adding all problems

--- Month 03/Week 02/Day 04/c.py
+++ Month 03/Week 02/Day 04/c.py
@@ -1,4 +1,42 @@
+# Design HashMap: https://leetcode.com/problems/design-hashmap/
 
+# Design a HashMap without using any built-in hash table libraries.
+
+# Implement the MyHashMap class:
+
+# MyHashMap() initializes the object with an empty map.
+# void put(int key, int value) inserts a (key, value) pair into the HashMap. If the key already exists in the map, update the corresponding value.
+# int get(int key) returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key.
+# void remove(key) removes the key and its corresponding value if the map contains the mapping for the key.
+
+class MyHashMap:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+
+    def put(self, key: int, value: int) -> None:
+        """
+        value will always be non-negative.
+        """
+
+    def get(self, key: int) -> int:
+        """
+        Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key
+        """
+
+    def remove(self, key: int) -> None:
+        """
+        Removes the mapping of the specified value key if this map contains a mapping for the key
+        """
+
+
+# Your MyHashMap object will be instantiated and called as such:
+# obj = MyHashMap()
+# obj.put(key,value)
+# param_2 = obj.get(key)
+# obj.remove(key)
 
 # Score Card
 # Did I need hints? Nope
