920ec3961cc5dac228265507853af439a4c2f79b
Kevin Knott <KKnoTT93@gmail.com>
2021-08-18 22:47:14 -0500

    Honestly this problem is just simple manipulation of strings and counting with a hash

--- Month 03/Week 02/Day 07/d.py
+++ Month 03/Week 02/Day 07/d.py
@@ -5,13 +5,39 @@
 #     For example, "9001 discuss.leetcode.com" is a count-paired domain that indicates that discuss.leetcode.com was visited 9001 times.
 # Given an array of count-paired domains cpdomains, return an array of the count-paired domains of each subdomain in the input. You may return the answer in any order.
 
+# So this is a basic parsing problem in which you parse the input and store in a hash map and then return that hashmap as a list
+
+from collections import defaultdict
+
+
 class Solution:
     def subdomainVisits(self, cpdomains):
-        return
+        ourMap = defaultdict(int)
+
+        for domain in cpdomains:
+            count, fullDomain = domain.split(' ')
+            count = int(count)
+
+            subDomain = fullDomain.split('.')
+            for i in range(len(subDomain)):
+                split = '.'.join(subDomain[i:])
+                ourMap[split] += count
+
+        result = []
+
+        for key, val in ourMap.items():
+            temp = ' '.join([str(val), key])
+            result.append(temp)
+
+        return result
+
+# The above works by basically spliting up the values to parse them correctly
+# and then adding each possible subdomain into the hash map to add our count
+# then we simply go through our map and add them to our result
 
 # Score Card
 # Did I need hints? N
-# Did you finish within 30 min? 10
+# Did you finish within 30 min? 7
 # Was the solution optimal? This is optimal
 # Were there any bugs? No
 #  5 5 5 5 = 5
