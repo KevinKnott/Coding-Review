3dd4160be7e71f0b7b2cde89c774e732b27a6b3c
Kevin Knott <KKnoTT93@gmail.com>
2021-08-18 21:13:04 -0500

    Adding all the problems for the day

---
+++ Month 03/Week 02/Day 07/a.py
@@ -0,0 +1,15 @@
+# Trapping Rain Water: https://leetcode.com/problems/trapping-rain-water/
+
+# Given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it can trap after raining.
+
+class Solution:
+    def trap(self, height) -> int:
+        return
+
+# Score Card
+# Did I need hints? N
+# Did you finish within 30 min? N (45 or so)
+# Was the solution optimal? I believe so we could make some slight optimization but this will run in o(n^2) because of the multiplicity we would go through once and then again to multiply
+#  and o(n) space
+# Were there any bugs? I listed bugs in the above code
+#  5 2 4 2 = 3.25
