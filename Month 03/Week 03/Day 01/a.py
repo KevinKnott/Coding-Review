cd30fd4b8e26766dabc446972d8627a88ae1e9c4
Kevin Knott <KKnoTT93@gmail.com>
2021-08-19 22:48:18 -0500

    First problem was an easy set using the int value as an index in set

--- Month 03/Week 03/Day 01/a.py
+++ Month 03/Week 03/Day 01/a.py
@@ -1,9 +1,47 @@
+# First Missing Positive: https://leetcode.com/problems/first-missing-positive/
 
+# Given an unsorted integer array nums, return the smallest missing positive integer.
+# You must implement an algorithm that runs in O(n) time and uses constant extra space.
+
+# This problem is relatively empty except we have quite a few false positives
+# so what we do is check if if 1 exists in the list if not we return false
+# otherwise we will set all negative numbers and 0 to 1 after handling all the
+# false positives we can set every possible number we see to negative if we see it
+# make sure we always take abs(i) - 1 and then we can go through afterwards
+# and look for the first positive number
+
+class Solution:
+    def firstMissingPositive(self, nums) -> int:
+        hasOne = False
+
+        for i in range(len(nums)):
+            if nums[i] == 1:
+                hasOne = True
+
+            if nums[i] <= 0:
+                nums[i] = 1
+
+        if not hasOne:
+            return 1
+
+        for i in range(len(nums)):
+            curNumIndex = abs(nums[i]) - 1
+
+            if curNumIndex < len(nums) and nums[curNumIndex] > 0:
+                nums[curNumIndex] *= -1
+
+        for i in range(1, len(nums)):
+            if nums[i] > 0:
+                return i+1
+
+        return len(nums) + 1
+
+
+# This runs in O(N) time and O(1) space
 
 # Score Card
 # Did I need hints? N
-# Did you finish within 30 min? N (45 or so)
-# Was the solution optimal? I believe so we could make some slight optimization but this will run in o(n^2) because of the multiplicity we would go through once and then again to multiply
-#  and o(n) space
-# Were there any bugs? I listed bugs in the above code
-#  5 2 4 2 = 3.25
+# Did you finish within 30 min? 10
+# Was the solution optimal? Oh yee
+# Were there any bugs? No bugs
+# 5 5 5 5 = 5
