4605fba4848c3ce229c8d842ca21ebe2496dd1b3
Kevin <KKnott93@gmail.com>
2021-08-20 21:21:37 -0500

    Add all the problem definitions

--- Month 03/Week 03/Day 02/c.py
+++ Month 03/Week 03/Day 02/c.py
@@ -1,4 +1,18 @@
+# All Nodes Distance K in Binary Tree: https://leetcode.com/problems/all-nodes-distance-k-in-binary-tree/
 
+# Given the root of a binary tree, the value of a target node target, and an integer k, return an array of the values of all nodes that have a distance k from the target node.
+# You can return the answer in any order.
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def distanceK(self, root: TreeNode, target: TreeNode, k: int) -> List[int]:
+        return
 
 # Score Card
 # Did I need hints? Nope
