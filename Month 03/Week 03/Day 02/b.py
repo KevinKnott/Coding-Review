bfa9bd38eca3ff4ec3afdc8d3cce6cb1e71fe078
Kevin <KKnott93@gmail.com>
2021-08-20 21:52:04 -0500

    Very tricky problem but knowing that you can swap around values in an array to make it an O(1) del is key

--- Month 03/Week 03/Day 02/b.py
+++ Month 03/Week 03/Day 02/b.py
@@ -8,32 +8,57 @@
 # int getRandom() Returns a random element from the current set of elements (it's guaranteed that at least one element exists when this method is called). Each element must have the same probability of being returned.
 # You must implement the functions of the class such that each function works in average O(1) time complexity.
 
+# By combining the properties of two data structures we can achieve what we need. Those two are a hashmap for quick lookup
+# and a array for storing the numbers to be accessed by a probability
+
+import random
+
+
 class RandomizedSet:
 
     def __init__(self):
         """
         Initialize your data structure here.
         """
-        
+        self.random = []
+        self.lookup = {}
 
     def insert(self, val: int) -> bool:
         """
         Inserts a value to the set. Returns true if the set did not already contain the specified element.
         """
-        
+        if val in self.lookup:
+            return False
+
+        # Add value to list and point the lookup to be the len as that is where it will be added
+        self.lookup[val] = len(self.random)
+        self.random.append(val)
+
+        return True
 
     def remove(self, val: int) -> bool:
         """
         Removes a value from the set. Returns true if the set contained the specified element.
         """
-        
+        if val not in self.lookup:
+            return False
+
+        # For this we need to remove the val but to do this in O(1) we need to swap the element
+        # to whatever is in the last place
+        last, remove = self.random[-1], self.lookup[val]
+        self.random[remove], self.lookup[last] = last, remove
+
+        self.random.pop()
+        del(self.lookup[val])
+
+        return True
 
     def getRandom(self) -> int:
         """
         Get a random element from the set.
         """
-        
-
+        index = random.randint(0, len(self.random) - 1)
+        return self.random[index]
 
 # Your RandomizedSet object will be instantiated and called as such:
 # obj = RandomizedSet()
@@ -41,10 +66,14 @@ class RandomizedSet:
 # param_2 = obj.remove(val)
 # param_3 = obj.getRandom()
 
+# Alright this works pretty well the tricky part is updating the random/lookup when removing from the set
+# because you have to update the location of the val so that you can properly remove it from both
+
+# This all runs in O(1)
 
 # Score Card
 # Did I need hints? Y
-# Did you finish within 30 min? Y
-# Was the solution optimal? Y although I messed up the thought process of how to solve the problem
-# Were there any bugs?  I didn't really have any bugs
-#  2 3 3 5 = 3.25
+# Did you finish within 30 min? 10
+# Was the solution optimal? Y
+# Were there any bugs? Messed up the swap piece real bad
+# 4 5 5 4 = 4.5
