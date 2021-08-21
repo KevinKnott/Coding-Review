6ba24e9131e7f57a99e7aadc5d5ca55e41e56777
Kevin <KKnott93@gmail.com>
2021-08-20 22:00:14 -0500

    Cruising through a tree problem by labeling parents and doing simple bfs for level order traversal with a twist

--- Month 03/Week 03/Day 02/c.py
+++ Month 03/Week 03/Day 02/c.py
@@ -4,19 +4,60 @@
 # You can return the answer in any order.
 
 # Definition for a binary tree node.
+
+from collections import deque
+
+
 class TreeNode:
     def __init__(self, x):
         self.val = x
         self.left = None
         self.right = None
 
+# This problem I think is relatively easy all we have to do is go through the tree twice
+# once by dfs to update add parent pointers and then once again with a bfs (bidirectional)
+# to find anything n nodes away
+
+
 class Solution:
     def distanceK(self, root: TreeNode, target: TreeNode, k: int):
-        return
+        def addParent(node, parent):
+            if node:
+                node.parent = parent
+
+                addParent(node.left, node)
+                addParent(node.right, node)
+
+        addParent(root, None)
+
+        q = deque()
+        q.appendleft([target, 0])
+        visited = set()
+        result = []
+
+        while q:
+            node, level = q.pop()
+
+            if node in visited or node is None:
+                continue
+
+            visited.add(node)
+
+            if level == k:
+                result.append(node.val)
+                continue
+            else:
+                q.appendleft([node.parent, level + 1])
+                q.appendleft([node.left, level + 1])
+                q.appendleft([node.right, level + 1])
+
+        return result
+
+# I was dead on this works in O(N) and o(N)
 
 # Score Card
 # Did I need hints? Nope
-# Did you finish within 30 min? 4 minutes
-# Was the solution optimal? Yup this runs in o(n+m) time in worst case and uses o(1) space
+# Did you finish within 30 min? 5 minutes
+# Was the solution optimal? Yup
 # Were there any bugs? None
 # 5 5 5 5 = 5
