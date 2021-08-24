4bc212e83de6a8b76226415b67d979070362d30c
Kevin <KKnott93@gmail.com>
2021-08-23 20:54:20 -0500

    An easy cheezy tree problem

--- Month 03/Week 03/Day 03/b.py
+++ Month 03/Week 03/Day 03/b.py
@@ -10,14 +10,33 @@ class TreeNode:
         self.left = left
         self.right = right
 
+# In this problem will take the tree and at every point make sure if we swap the left and the right
+# that they both return the reccurnce relationship that they are equal (the left and right being swapped)
+
 
 class Solution:
     def isSymmetric(self, root):
-        return
+        if root is None:
+            return
+
+        def swapSides(node1, node2):
+            if node1 and node2:
+                return node1.val == node2.val and swapSides(node1.left, node2.right) and swapSides(node1.right, node2.left)
+            if node1 and not node2:
+                return False
+            if node2 and not node1:
+                return False
+
+            return True
+
+        return swapSides(root, root)
+
+# This works and is basically making sure you understand how exactly you can move through trees with a recurrence
+# Our code runs in O(N) as it will have to visit every node
 
 # Score Card
 # Did I need hints? Y
-# Did you finish within 30 min? Y
-# Was the solution optimal? Y although I messed up the thought process of how to solve the problem
-# Were there any bugs?  I didn't really have any bugs
-#  2 3 3 5 = 3.25
+# Did you finish within 30 min? 5
+# Was the solution optimal? Y
+# Were there any bugs? N
+# 5 5 5 5 = 5
