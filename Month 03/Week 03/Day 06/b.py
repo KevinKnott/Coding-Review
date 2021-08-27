8ae7378ff365583ab736a19a807d5d94d953e980
Kevin <KKnott93@gmail.com>
2021-08-26 20:40:07 -0500

    Crusing through a linked list reversal that has a bit more to it

--- Month 03/Week 03/Day 06/b.py
+++ Month 03/Week 03/Day 06/b.py
@@ -12,15 +12,55 @@ class ListNode:
         self.val = val
         self.next = next
 
+# In this problem all we have to do is traverse until we find left and then go ahead and reverse every node up to the right
+# the only trick to keep note of is if the head node is the left we will also need to reassing our head  node.
+
 
 class Solution:
     def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
-        return
+        if head is None:
+            return
 
+        if left == right:
+            return head
+
+        cur = head
+        prev = None
+
+        # We want to keep the previous node so we go until left == 1 (indexed from 1 as well)
+        while left > 1:
+            prev = cur
+            cur = cur.next
+            left -= 1
+            right -= 1
+
+        # Now to reverse (we know the tail will end up in our cur pointer and that we need to connect the previous to the reversed nodes)
+        tail, connection = cur, prev
+
+        while right > 0:
+            temp = cur.next
+            cur.next = prev
+            prev = cur
+            cur = temp
+            right -= 1
+
+        # Now we know if our connector is None that we actually started reversing at the first node so we will update head
+        if connection is None:
+            head = prev
+        else:
+            connection.next = prev
+
+        # Now we need to make sure that our tail of the swapped node points to the next node
+        tail.next = cur
+
+        return head
+
+# This runs in O(N) as we will only traverse once and uses O(1) as we simply are reversing in place
+# I think you could have also solved this with backtracking but it is more complicated
 
 # Score Card
-# Did I need hints? Y
-# Did you finish within 30 min? Y
-# Was the solution optimal? Y although I messed up the thought process of how to solve the problem
-# Were there any bugs?  I didn't really have any bugs
-#  2 3 3 5 = 3.25
+# Did I need hints? N
+# Did you finish within 30 min? 10
+# Was the solution optimal? Ye
+# Were there any bugs? Ne
+# 5 5 5 5 = 5
