483b85c15fa9e372cf506370f01eac270fcf3c13
Kevin <KKnott93@gmail.com>
2021-08-27 21:53:51 -0500

    Cheecky math problem boiled down to a stack problem

--- Month 03/Week 03/Day 07/a.py
+++ Month 03/Week 03/Day 07/a.py
@@ -3,14 +3,53 @@
 # Given a string s representing a valid expression, implement a basic calculator to evaluate it, and return the result of the evaluation.
 # Note: You are not allowed to use any built-in function which evaluates strings as mathematical expressions, such as eval().
 
+# In this problem everything is simple enough and there are two approaches but both are stack based you can use recursion when you
+# see a paren and evaluate everything within as we only have + and - and then return the result and evaluate as you go. Alternatively
+# You could do a stack based solution in which you iteratively do the same and then use a stack to keep on the value and the sign (+/-)
+
 class Solution:
     def calculate(self, s: str) -> int:
-        return
+        stack = []
+        sign = 1
+        result = 0
+        operand = 0
+
+        for char in s:
+
+            if char.isdigit():
+                operand = (operand * 10) + int(char)
+
+            elif char == '+':
+                result += sign * operand
+                operand = 0
+                sign = 1
+
+            elif char == '-':
+                result += sign * operand
+                operand = 0
+                sign = -1
+
+            elif char == '(':
+                stack.append(result)
+                stack.append(sign)
+
+                result = 0
+                sign = 1
+
+            elif char == ')':
+                result += sign * operand
+                result *= stack.pop()
+                result += stack.pop()
+
+                operand = 0
+
+        return result + (sign * operand)
+
+# This works and is pretty simple it runs in O(N) and can use up to O(N) space as well
 
 # Score Card
 # Did I need hints? N
-# Did you finish within 30 min? N (45 or so)
-# Was the solution optimal? I believe so we could make some slight optimization but this will run in o(n^2) because of the multiplicity we would go through once and then again to multiply
-#  and o(n) space
-# Were there any bugs? I listed bugs in the above code
-#  5 2 4 2 = 3.25
+# Did you finish within 30 min? 10
+# Was the solution optimal? Oh yeah
+# Were there any bugs? None
+# 5 5 5 5 = 5
