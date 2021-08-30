192b5eb4f40218d1799a956c77961c0c17501941
Kevin <KKnott93@gmail.com>
2021-08-30 18:08:13 -0500

    Calculator problem in which we convert the problem first to a stack for parsing then reduce space with clever var manipulation

--- Month 03/Week 04/Day 01/a.py
+++ Month 03/Week 04/Day 01/a.py
@@ -5,14 +5,88 @@
 # You may assume that the given expression is always valid. All intermediate results will be in the range of [-231, 231 - 1].
 # Note: You are not allowed to use any built-in function which evaluates strings as mathematical expressions, such as eval().
 
+# In this problem we have two options for what we can do: either we go throught he string and simple make all additions/subtractions
+# happen at the end with a stack and solve for any * or / immediately. Alternatively you can use an extra variable to keep track
+# of the last variable as any time you need to use * or / it will only go back one and if not you can simply calculate as you go
+# through the string
+
 class Solution:
     def calculate(self, s: str) -> int:
-        return
+        if len(s) == 0:
+            return 0
+
+        stack = []
+        operand = 0
+        sign = '+'
+
+        for i in range(len(s)):
+            ch = s[i]
+
+            if ch.isdigit():
+                operand = (operand * 10) + int(ch)
+
+            if ch in '+-/*' or i == len(s) - 1:
+                if sign == '+':
+                    stack.append(operand)
+                elif sign == '-':
+                    stack.append(-operand)
+                elif sign == '*':
+                    stack.append(stack.pop() * operand)
+                else:
+                    stack.append(int(stack.pop()/operand))
+
+                operand = 0
+                sign = ch
+
+        result = 0
+
+        while stack:
+            result += stack.pop()
+
+        return result
+
+# So this works and is actually pretty optimal it runs in O(N) time and space but we can do better as I said.
+# As you know I can just keep track of the last which would be equivalent to the stack.pop() above
+
+    def calculate(self, s: str) -> int:
+        if len(s) == 0:
+            return 0
+
+        result = 0
+        last, cur = 0, 0
+        sign = '+'
+
+        for i in range(len(s)):
+            ch = s[i]
+
+            if ch.isdigit():
+                cur = (cur * 10) + int(ch)
+
+            if ch in '+-/*' or i == len(s) - 1:
+                if sign == '+':
+                    result += last
+                    last = cur
+                elif sign == '-':
+                    result += last
+                    last = -cur
+                elif sign == '*':
+                    last = last * cur
+                else:
+                    last = int(last / cur)
+
+                cur = 0
+                sign = ch
+
+        result += last
+
+        return result
+
+# Aaaand boom a sexy transformation which saves us on space complexity.
+# This will run in O(N) time and O(1) space as we are calculating on the fly
 
 # Score Card
 # Did I need hints? N
-# Did you finish within 30 min? N (45 or so)
-# Was the solution optimal? I believe so we could make some slight optimization but this will run in o(n^2) because of the multiplicity we would go through once and then again to multiply
-#  and o(n) space
-# Were there any bugs? I listed bugs in the above code
-#  5 2 4 2 = 3.25
+# Did you finish within 30 min? 17
+# Was the solution optimal? Oh yeah
+# Were there any bugs? Nope
+# 5 5 5 5 = 5
