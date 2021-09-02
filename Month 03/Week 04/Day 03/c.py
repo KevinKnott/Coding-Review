41058a1db982c1eee274a69c346c6fb828ca36af
Kevin <KKnott93@gmail.com>
2021-09-01 19:42:03 -0500

    Merge sort with linked lists

--- Month 03/Week 04/Day 03/c.py
+++ Month 03/Week 04/Day 03/c.py
@@ -13,14 +13,58 @@ class ListNode:
         self.val = val
         self.next = next
 
+# This problem is actually just a merge sort no need to explain really
+# There are two solutions one with a for loop that creates a new list of ll
+# and one that simply merges the current list which saves us o (N) space
+# to become O(1)
+
 
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        return
+        if lists is None or len(lists) == 0:
+            return None
+
+        interval = 1
+        total = len(lists)
+
+        while interval < total:
+            for i in range(0, total - interval, interval * 2):
+                lists[i] = self.merge2(lists[i], lists[i + interval])
+
+            interval *= 2
+
+        return lists[0] if total > 0 else []
+
+    def merge2(self, l1, l2):
+        dummy = ListNode()
+        cur = dummy
+
+        while l1 and l2:
+            if l1.val < l2.val:
+                cur.next = l1
+                l1 = l1.next
+            else:
+                cur.next = l2
+                l2 = l2.next
+
+            cur = cur.next
+
+        if l1:
+            cur.next = l1
+        else:
+            cur.next = l2
+
+        return dummy.next
+
+# This works although I forgot that my range needs to decrease then number of nodes to review
+# by the number of merged (going up 2 at a time)
+
+# This runs in o(nlogk) where k is the number of ll and uses O(1) additional space
+
 
 # Score Card
 # Did I need hints? Nope
-# Did you finish within 30 min? 4 minutes
-# Was the solution optimal? Yup this runs in o(n+m) time in worst case and uses o(1) space
+# Did you finish within 30 min? 12
+# Was the solution optimal? Yup
 # Were there any bugs? None
 # 5 5 5 5 = 5
