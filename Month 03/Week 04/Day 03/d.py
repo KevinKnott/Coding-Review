ef9dfca4985b7064d62ba7043bfa74652e4affd2
Kevin <KKnott93@gmail.com>
2021-09-01 19:49:50 -0500

    Heaps that solve sorting problems

--- Month 03/Week 04/Day 03/d.py
+++ Month 03/Week 04/Day 03/d.py
@@ -5,14 +5,40 @@
 
 from types import List
 
+# So the first solution I think of in this problem is to use a heap and simply pop k nodes of in o(klogk) time (its a bit complicated
+# so I may be a bit wrong). I think this could also be solved using a binary search but I am not exactly sure how to do it
+
+
+import heapq
+
 
 class Solution:
     def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
-        return
+        ourHeap = []
+        for i in range(len(matrix)):
+            ourHeap.append((matrix[i][0], i, 0))
+
+        heapq.heapify(ourHeap)
+        size = len(matrix[0])
+
+        # reduce to only one above k so pop returns answer
+        while k > 1:
+            _, r, c = heapq.heappop(ourHeap)
+
+            if c + 1 < size:
+                heapq.heappush(ourHeap, (matrix[r][c+1], r, c+1))
+
+            k -= 1
+
+        result, _, _ = heapq.heappop(ourHeap)
+
+        return result
+
+# This works and is pretty optimal see above
 
 # Score Card
 # Did I need hints? N
-# Did you finish within 30 min? 10
+# Did you finish within 30 min? 6
 # Was the solution optimal? This is optimal
 # Were there any bugs? No
 #  5 5 5 5 = 5
