625d951082d20b0abfee27df7edea8a6803392d5
Kevin <KKnott93@gmail.com>
2021-09-02 23:00:41 -0500

    Second problem was a dfs problem where you need to manipulate pointers

--- Month 03/Week 04/Day 04/b.py
+++ Month 03/Week 04/Day 04/b.py
@@ -12,14 +12,48 @@ class Node:
         self.left = left
         self.right = right
 
+# In this problem all we have to do is a dfs as a the left most node will be the head and the right most node will be the tail
+# then all we have to do is keep two variables outside of the loop or in the parameters. They will keep track of the prev
+# node and the first node. That way we can link prev to next and cur to prev as well as at the very end point first
+# to last and vice versa
+
 
 class Solution:
     def treeToDoublyList(self, root: 'Node') -> 'Node':
-        return
+        if not root:
+            return
+
+        self.first, self.prev = None, None
+
+        def dfs(node):
+            if node.left:
+                dfs(node.left)
+
+            if self.first is None:
+                self.first = node
+
+            if self.prev:
+                self.prev.right = node
+                node.left = self.prev
+
+            self.prev = node
+
+            if node.right:
+                dfs(node.right)
+
+        dfs(root)
+
+        self.first.left = self.prev
+        self.prev.right = self.first
+        return self.first
+
+
+# Eyyy another smooth problem. This one is relatively easy if you consider what needs to be done
+# It runs in O(N) and uses O(1) space.
 
 #  Score Card
-# Did I need hints? Y
-# Did you finish within 30 min? Y
-# Was the solution optimal? Y although I messed up the thought process of how to solve the problem
-# Were there any bugs?  I didn't really have any bugs
-#  2 3 3 5 = 3.25
+# Did I need hints? N
+# Did you finish within 30 min? 10
+# Was the solution optimal? Y
+# Were there any bugs? N
+# 5 5 5 5 = 5
