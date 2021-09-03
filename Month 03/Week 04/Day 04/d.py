cf13712ed47ee7f28a5478a2ed0f32bca40f0ddb
Kevin <KKnott93@gmail.com>
2021-09-02 23:18:25 -0500

    parsing a string and adding it to a dict which is super easy

--- Month 03/Week 04/Day 04/d.py
+++ Month 03/Week 04/Day 04/d.py
@@ -8,16 +8,39 @@
 # Given an array of count-paired domains cpdomains, return an array of the count-paired domains of each subdomain in the input. You may return the answer in any order.
 
 from types import List
+from collections import defaultdict
 
 
 class Solution:
     def subdomainVisits(self, cpdomains: List[str]) -> List[str]:
-        return
+        ourCount = defaultdict(int)
 
+        for domain in cpdomains:
+            count, fullDomain = domain.split(" ")
+            count = int(count)
+
+            subDomain = fullDomain.split('.')
+            for i in range(len(subDomain)):
+                split = '.'.join(subDomain[i:])
+                ourCount[split] += count
+
+        result = []
+
+        for k, v in ourCount.items():
+            temp = ' '.join([str(v), k])
+            result .append(temp)
+
+        return result
+
+
+# Oh yeah this works like a charm
+# Honestly it simply just parsing a string and adding it to ta count which is super easy
+# This runs in O(N) time and space although splicing the strings for the key is quite
+# a heavy operation
 
 # Score Card
 # Did I need hints? N
-# Did you finish within 30 min? 10
+# Did you finish within 30 min? 6
 # Was the solution optimal? This is optimal
 # Were there any bugs? No
 #  5 5 5 5 = 5
