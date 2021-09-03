d42c23bff5a49747b26336e73ff42530f96340bf
Kevin <KKnott93@gmail.com>
2021-09-03 18:01:26 -0500

    A string problem that is actually a stack problem, where you can find invalid chars by using a stack to parse parens

--- Month 03/Week 04/Day 05/c.py
+++ Month 03/Week 04/Day 05/c.py
@@ -10,14 +10,40 @@
 # It can be written as AB (A concatenated with B), where A and B are valid strings, or
 # It can be written as (A), where A is a valid string.
 
+# In this problem we need to parse through the string and keep track of where valid parens are and remove them
+# Anytime we have an invalid ) we can tell as there won't be  ( to catch it and so we can mark the index
+# if there are valid we simply remove ( index and at the end we re parse the string and skip over
+# any value in our stack
+
 class Solution:
     def minRemoveToMakeValid(self, s: str) -> str:
-        return
+        stack = []
+
+        for i in range(len(s)):
+            if s[i] == '(':
+                stack.append(i)
+
+            elif s[i] == ')':
+                if stack and s[stack[-1]] == '(':
+                    stack.pop()
+                else:
+                    stack.append(i)
+
+        toRemove = set(stack)
+        result = ""
+
+        for i in range(len(s)):
+            if i not in toRemove:
+                result += s[i]
+
+        return result
+
+# This works and runs in O(N) time and space
 
 
 # Score Card
 # Did I need hints? Nope
-# Did you finish within 30 min? 4 minutes
-# Was the solution optimal? Yup this runs in o(n+m) time in worst case and uses o(1) space
+# Did you finish within 30 min? 8
+# Was the solution optimal? Yup
 # Were there any bugs? None
 # 5 5 5 5 = 5
