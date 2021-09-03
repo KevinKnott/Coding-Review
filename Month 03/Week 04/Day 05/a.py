3db6f1753809dca539287c6ad05ecf7e149c7d2d
Kevin <KKnott93@gmail.com>
2021-09-03 17:07:49 -0500

    Adding all of the problems for the day, although I may not get to all of them as I am heading out for vacay

---
+++ Month 03/Week 04/Day 05/a.py
@@ -0,0 +1,45 @@
+# Serialize and Deserialize Binary Tree: https://leetcode.com/problems/serialize-and-deserialize-binary-tree/
+
+# Serialization is the process of converting a data structure or object into a sequence of bits so that it can be stored in a file or memory buffer, or transmitted across a network connection link to be reconstructed later in the same or another computer environment.
+# Design an algorithm to serialize and deserialize a binary tree. There is no restriction on how your serialization/deserialization algorithm should work. You just need to ensure that a binary tree can be serialized to a string and this string can be deserialized to the original tree structure.
+# Clarification: The input/output format is the same as how LeetCode serializes a binary tree. You do not necessarily need to follow this format, so please be creative and come up with different approaches yourself.
+
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Codec:
+
+    def serialize(self, root):
+        """Encodes a tree to a single string.
+
+        :type root: TreeNode
+        :rtype: str
+        """
+        return
+
+    def deserialize(self, data):
+        """Decodes your encoded data to tree.
+
+        :type data: str
+        :rtype: TreeNode
+        """
+        return
+
+# Your Codec object will be instantiated and called as such:
+# ser = Codec()
+# deser = Codec()
+# ans = deser.deserialize(ser.serialize(root))
+
+
+# Score Card
+# Did I need hints? N
+# Did you finish within 30 min? N (45 or so)
+# Was the solution optimal? I believe so we could make some slight optimization but this will run in o(n^2) because of the multiplicity we would go through once and then again to multiply
+#  and o(n) space
+# Were there any bugs? I listed bugs in the above code
+#  5 2 4 2 = 3.25
---
+++ Month 03/Week 04/Day 05/b.py
@@ -0,0 +1,26 @@
+# All Nodes Distance K in Binary Tree: https://leetcode.com/problems/all-nodes-distance-k-in-binary-tree/
+
+# Given the root of a binary tree, the value of a target node target, and an integer k, return an array of the values of all nodes that have a distance k from the target node.
+# You can return the answer in any order.
+
+# Definition for a binary tree node.
+from types import List
+
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def distanceK(self, root: TreeNode, target: TreeNode, k: int) -> List[int]:
+        return
+
+# Score Card
+# Did I need hints? Y
+# Did you finish within 30 min? Y
+# Was the solution optimal? Y although I messed up the thought process of how to solve the problem
+# Were there any bugs?  I didn't really have any bugs
+#  2 3 3 5 = 3.25
