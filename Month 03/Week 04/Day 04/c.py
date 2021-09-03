1c7a75353522fa414370c1eca2ae84768b575bfa
Kevin <KKnott93@gmail.com>
2021-09-02 22:21:49 -0500

    Add problems for the day

---
+++ Month 03/Week 04/Day 04/c.py
@@ -0,0 +1,18 @@
+# Merge Intervals: https://leetcode.com/problems/merge-intervals/
+
+# Given an array of intervals where intervals[i] = [starti, endi], merge all overlapping intervals, and return an array of the non-overlapping intervals that cover all the intervals in the input.
+
+from types import List
+
+
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        return
+
+
+# Score Card
+# Did I need hints? Nope
+# Did you finish within 30 min? 4 minutes
+# Was the solution optimal? Yup this runs in o(n+m) time in worst case and uses o(1) space
+# Were there any bugs? None
+# 5 5 5 5 = 5
