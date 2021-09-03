42b46b4c621b58cefc4558e72f791dfaf4177de4
Kevin <KKnott93@gmail.com>
2021-09-03 17:47:24 -0500

    Another q problem but this time we need to label trees to make sure we can go up and down tree

--- Month 03/Week 04/Day 05/b.py
+++ Month 03/Week 04/Day 05/b.py
@@ -5,6 +5,7 @@
 
 # Definition for a binary tree node.
 from types import List
+from collections import deque
 
 
 class TreeNode:
@@ -13,14 +14,60 @@ class TreeNode:
         self.left = None
         self.right = None
 
+# All we have to do for this problem is iterate through it twice once to set the parent attribute in a treenode
+# and then again to do a bfs in both directions!
+
 
 class Solution:
     def distanceK(self, root: TreeNode, target: TreeNode, k: int) -> List[int]:
-        return
+        if not root:
+            return []
+
+        def markParent(node, parent):
+            node.parent = parent
+
+            if node.left:
+                markParent(node.left, node)
+
+            if node.right:
+                markParent(node.right, node)
+
+        markParent(root, None)
+
+        q = deque()
+        q.appendleft((target, 0))
+        result = []
+        visited = set()
+
+        while q:
+            node, level = q.pop()
+
+            if node.val in visited or level > k:
+                continue
+
+            visited.add(node.val)
+
+            if level == k:
+                result.append(node.val)
+                continue
+
+            if node.parent:
+                q.appendleft((node.parent, level + 1))
+
+            if node.left:
+                q.appendleft((node.left, level + 1))
+
+            if node.right:
+                q.appendleft((node.right, level + 1))
+
+        return result
+
+# Boom
+# This works and runs in O(N) (2N) and uses O(N) space for the q
 
 # Score Card
 # Did I need hints? Y
-# Did you finish within 30 min? Y
-# Was the solution optimal? Y although I messed up the thought process of how to solve the problem
-# Were there any bugs?  I didn't really have any bugs
-#  2 3 3 5 = 3.25
+# Did you finish within 30 min? 6
+# Was the solution optimal? Y
+# Were there any bugs? None
+# 5 5 5 5 = 5
