b98346fdf59be53abc5a7b63afdc8a3764c8a308
Kevin <KKnott93@gmail.com>
2021-09-03 17:34:24 -0500

    First problem is a serialization problem which can be solved with a queue!

--- Month 03/Week 04/Day 05/a.py
+++ Month 03/Week 04/Day 05/a.py
@@ -5,11 +5,17 @@
 # Clarification: The input/output format is the same as how LeetCode serializes a binary tree. You do not necessarily need to follow this format, so please be creative and come up with different approaches yourself.
 
 # Definition for a binary tree node.
+from collections import deque
+
+
 class TreeNode(object):
     def __init__(self, x):
         self.val = x
         self.left = None
         self.right = None
+
+# In this problem we are encoding and decoding a tree into a list. To do this we can simply use a BFS technique and anytime we see a None value we simply push on a string None
+# Then to decode we do the same thing in reverse.
 
 
 class Codec:
@@ -20,7 +26,25 @@ class Codec:
         :type root: TreeNode
         :rtype: str
         """
-        return
+
+        if not root:
+            return ''
+
+        encoded = []
+        q = deque()
+        q.appendleft(root)
+
+        while q:
+            node = q.pop()
+
+            if node is None or node == "None":
+                encoded.append("None")
+            else:
+                encoded.append(str(node.val))
+                q.appendleft(node.left)
+                q.appendleft(node.right)
+
+        return ' '.join(encoded)
 
     def deserialize(self, data):
         """Decodes your encoded data to tree.
@@ -28,18 +52,50 @@ class Codec:
         :type data: str
         :rtype: TreeNode
         """
-        return
+        if len(data) == 0:
+            return
+
+        data = data.split(' ')
+
+        i = 1
+        total = len(data)
+        head = TreeNode(int(data[0]))
+
+        q = deque()
+        q.appendleft(head)
+
+        while i < total and q:
+            node = q.pop()
+
+            # Append left
+            if data[i] != 'None':
+                left = TreeNode(int(data[i]))
+                node.left = left
+                q.appendleft(left)
+
+            i += 1
+
+            # Append right
+            if data[i] != 'None':
+                right = TreeNode(int(data[i]))
+                node.right = right
+                q.appendleft(right)
+
+            i += 1
+
+        return head
 
 # Your Codec object will be instantiated and called as such:
 # ser = Codec()
 # deser = Codec()
 # ans = deser.deserialize(ser.serialize(root))
 
+# So I am pretty sure I have the correct solution and it should run in O(N) and uses O(N) additional space
+
 
 # Score Card
 # Did I need hints? N
-# Did you finish within 30 min? N (45 or so)
-# Was the solution optimal? I believe so we could make some slight optimization but this will run in o(n^2) because of the multiplicity we would go through once and then again to multiply
-#  and o(n) space
-# Were there any bugs? I listed bugs in the above code
-#  5 2 4 2 = 3.25
+# Did you finish within 30 min? 12
+# Was the solution optimal? Y
+# Were there any bugs? N
+# 5 5 5 5 = 5
